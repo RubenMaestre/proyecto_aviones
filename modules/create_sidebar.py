00415e01f3f2f4db9d055951a2aecdaefc5d3719
RubenMaestre <formacion@rubenmaestre.com>
2024-03-31 20:11:22 +0200

    Update create_sidebar.py

--- modules/create_sidebar.py
+++ modules/create_sidebar.py
@@ -4,45 +4,9 @@ from streamlit_option_menu import option_menu
 from paginas import inicio, eda, modelo, sobre_nosotros
 
 def create_sidebar():
-     # Aplicar estilos CSS personalizados para el sidebar
-    st.markdown("""
-        <style>
-            /* Estilos para el fondo y el texto del sidebar */
-            [data-testid="stSidebar"] .css-1e5imcs {
-                background-color: black !important;
-            }
-            [data-testid="stSidebar"] .css-1e5imcs * {
-                color: white !important;
-            }
-            
-            /* Ajustar el color de texto de todos los elementos dentro del sidebar */
-            [data-testid="stSidebar"] .st-bq, 
-            [data-testid="stSidebar"] .st-cx {
-                color: white !important;
-            }
-            
-            /* Estilos específicos para el título y botones del sidebar */
-            [data-testid="stSidebar"] .st-cx {
-                text-align: center !important;
-                margin-top: 30px !important;
-            }
-            
-            [data-testid="stSidebar"] .stButton>button {
-                display: block;
-                margin: 10px auto;
-                width: 90%;
-            }
-            
-            /* Ajustes para el título del sidebar */
-            .css-1v3fvcr {
-                margin-bottom: 30px !important;
-            }
-        </style>
-        """, unsafe_allow_html=True)
-
     # Añadir texto personalizado en el sidebar con markdown y HTML
     st.sidebar.markdown(
-        f'<div style="text-align: center; color: white; font-size: 18px; margin-bottom: 30px;">'
+        f'<div style="text-align: center; font-size: 18px; margin-bottom: 30px;">'
         f'Proyecto realizado por<br>'
         f'José Núñez, Rubén Maestre, Dafne Moreno y Nahuel Núñez'
         f'</div>',
