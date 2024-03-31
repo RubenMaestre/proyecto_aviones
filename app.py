90226fed0987eedb9efbf8f91afcca3e8f028388
RubenMaestre <formacion@rubenmaestre.com>
2024-03-31 19:28:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from menu import create_sidebar
 # Configuración de la página, incluyendo el favicon
 st.set_page_config(
     page_title="Análisis de Puntualidad en Aeropuertos",  # Título de la pestaña del navegador
-    page_icon="images/favicon.png",  # Ruta al archivo del favicon
+    page_icon="✈️",  # Emoji de avión como favicon
     layout="wide"  # Opcional: Configura el layout de la página a "wide"
 )
 # Puedes llamar aquí a cualquier configuración global adicional si es necesario
