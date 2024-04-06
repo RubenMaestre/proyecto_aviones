a61e289d12b7aebbf3a46076c220d57faacb83ba
RubenMaestre <formacion@rubenmaestre.com>
2024-04-06 19:13:11 +0200

    Update vuelos.py

--- paginas/vuelos.py
+++ paginas/vuelos.py
@@ -14,25 +14,13 @@ def display():
     st.title('Vuelos en USA')
 
     # Crear el mapa base con Folium
-    mapa = folium.Map(location=[40, -95], zoom_start=2)  # Ajusta según tus necesidades
+    mapa = folium.Map(location=[48.8566, 2.3522], zoom_start=2)  
 
     # Añadir marcadores para cada aeropuerto (este es solo un ejemplo, ajusta según tu DataFrame)
     for index, row in df_aeropuertos_unicos.iterrows():
         folium.Marker([row['latitude'], row['longitude']], popup=row['nombre_aeropuerto']).add_to(mapa)
 
     st_folium(mapa, width=1280, height=720)
-
-def display_map(filtered_df):
-    # Crear un mapa base con Folium
-    mapa = folium.Map(location=[40, -95], zoom_start=4)
-
-    # Añadir marcadores para cada aeropuerto en el DataFrame filtrado
-    for index, row in filtered_df.iterrows():
-        folium.Marker([row['latitude_origen'], row['longitude_origen']], 
-                      popup=f"{row['aeropuerto_origen']} ({row['ciudad_origen']}, {row['estado_origen']})").add_to(mapa)
-
-    # Mostrar el mapa en Streamlit
-    st_folium(mapa, width=725, height=500)
 
     # Llamar a display() para mostrar el primer mapa
     display()
