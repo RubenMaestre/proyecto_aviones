226364150ee4036fb85fa32def2e5c96b8dce760
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 10:12:18 +0200

    actualiza

--- modules/create_sidebar.py
+++ modules/create_sidebar.py
@@ -1,7 +1,7 @@
 # modules/create_sidebar.py
 import streamlit as st
 from streamlit_option_menu import option_menu
-from paginas import inicio, eda, aviones, modelo, sobre_nosotros, sobre_proyecto
+from paginas import inicio, datos, eda, aviones, modelo, sobre_nosotros, sobre_proyecto
 from paginas.vuelos import aerolineas, aeropuertos, datos
 
 def create_sidebar():
@@ -16,13 +16,15 @@ def create_sidebar():
 
     # Crear el menú de opciones en el sidebar con option_menu
     with st.sidebar:
-        selected = option_menu("Menú del proyecto", ["Inicio", "EDA", "Vuelos en USA", "Modelo machine learning", "Sobre el proyecto", "Sobre nosotros"],
-            icons=["house", "bar-chart-line", "airplane", "cpu", "book", "people"],
+        selected = option_menu("Menú del proyecto", ["Inicio", "Datos", "EDA", "Vuelos en USA", "Modelo machine learning", "Sobre el proyecto", "Sobre nosotros"],
+            icons=["house", "disk", "bar-chart-line", "airplane", "cpu", "book", "people"],
             menu_icon="cast", default_index=0, orientation="vertical")
 
     # Llama a la función de la página correspondiente en función de la selección
     if selected == "Inicio":
         inicio.display()
+    elif selected == "Los datos":
+        datos.display()
     elif selected == "EDA":
         eda.display()
     elif selected == "Vuelos en USA":
