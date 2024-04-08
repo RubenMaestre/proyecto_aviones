2682dc489ee57341b9532c976c0f8fe04202d5fc
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 21:32:19 +0200

    actualiza

--- paginas/eda.py
+++ paginas/eda.py
@@ -1,7 +1,6 @@
 # paginas/eda.py
 import streamlit as st
 from modules.botones_eda import crear_botones_eda  # Importa la función de botones
-from modules.cargar_unir_df import unir_dataframes_pickle  # Asume que esta es la ubicación de tu función
 
 # Importa las funciones display de cada año y TODOS
 from paginas.anual.todos import display as display_todos
@@ -11,10 +10,6 @@ from paginas.anual.dec_2021 import display as display_2021
 
 def display():
     st.title('Exploración de Datos de Vuelos')
-
-    # Llamada a la función para cargar y unir los DataFrames pickle
-    directorio_pickle = 'data/pickles'  # Asegúrate de que esta sea la ruta correcta
-    df_vuelos_limpio = unir_dataframes_pickle(directorio_pickle)
 
     # Llama a la función para mostrar los botones de navegación
     crear_botones_eda()
@@ -26,13 +21,18 @@ def display():
     # Navegación interna basada en el estado de la sesión
     if 'subpagina_eda' in st.session_state:
         if st.session_state.subpagina_eda == 'todos':
-            display_todos(df_vuelos_limpio)  # Pasar el DataFrame unido a la función
+            # Llama a la función para mostrar los datos de TODOS los años
+            display_todos()
         elif st.session_state.subpagina_eda == '2023':
-            display_2023(df_vuelos_limpio)  # Pasar el DataFrame unido a la función
+            # Llama a la función para mostrar los datos del año 2023
+            display_2023()
         elif st.session_state.subpagina_eda == '2022':
-            display_2022(df_vuelos_limpio)  # Pasar el DataFrame unido a la función
+            # Llama a la función para mostrar los datos del año 2022
+            display_2022()
         elif st.session_state.subpagina_eda == '2021':
-            display_2021(df_vuelos_limpio)  # Pasar el DataFrame unido a la función
+            # Llama a la función para mostrar los datos del año 2021
+            display_2021()
 
 # Llama a la función para mostrar la página
 display()
+
