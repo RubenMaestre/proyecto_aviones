6e33600a8f305cf61f4cc5f0c981bd1dc23bfa2c
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 18:37:02 +0200

    Update botones_aviones.py

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -4,20 +4,46 @@ import streamlit as st
 def crear_botones():
     col1, col2, col3, col4 = st.columns(4)
 
-    # Define el enlace de cada imagen. En este caso, estamos simulando una acción de "navegación"
-    # al establecer una clave en st.session_state. La clave debería manejarse en tu app principal.
-    def markdown_image(image_path, key, caption=""):
-        return st.markdown(f"<div style='text-align: center;'><a href='javascript:void(0);' onclick='localStorage.setItem(\"{key}\", \"true\");'><img src='{image_path}' class='img-fluid' width='200'><p>{caption}</p></a></div>", unsafe_allow_html=True)
+    # CSS para usar imágenes como fondo de botones
+    button_style = """
+    <style>
+        .btn {
+            border: none;
+            color: white;
+            padding: 0px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            width: 200px;
+            height: 200px;
+        }
+        .btn1 {background-image: url('sources/aviones.jpg'); background-size: cover;}
+        .btn2 {background-image: url('sources/aeropuertos.jpg'); background-size: cover;}
+        .btn3 {background-image: url('sources/aerolineas.jpg'); background-size: cover;}
+        .btn4 {background-image: url('sources/datos.jpg'); background-size: cover;}
+    </style>
+    """
+
+    # Asegúrate de que las URLs de las imágenes sean accesibles públicamente
+    def markdown_button(button_class, key):
+        # Al hacer clic en el botón, se actualizará st.session_state.subpagina
+        return st.markdown(f"<button class='btn {button_class}' onclick='if(typeof(Storage) !== \"undefined\") {{localStorage.setItem(\"{key}\", \"true\"); location.reload(); }}'></button>", unsafe_allow_html=True)
 
     with col1:
-        markdown_image("sources/aviones.jpg", 'vuelos_usa', "Ver vuelos en USA")
+        markdown_button("btn1", "vuelos_usa")
 
     with col2:
-        markdown_image("sources/aeropuertos.jpg", 'aeropuertos', "Ver aeropuertos de USA")
+        markdown_button("btn2", "aeropuertos")
 
     with col3:
-        markdown_image("sources/aerolineas.jpg", 'aerolineas', "Ver aerolíneas de USA")
+        markdown_button("btn3", "aerolineas")
 
     with col4:
-        markdown_image("sources/datos.jpg", 'datos', "Ver datos de vuelos USA")
+        markdown_button("btn4", "datos")
+
+    # Aplica el estilo CSS
+    st.markdown(button_style, unsafe_allow_html=True)
 
