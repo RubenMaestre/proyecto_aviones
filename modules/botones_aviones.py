42c9df5206ea6d8c0af77f5f5586d887df897183
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 18:52:29 +0200

    Update botones_aviones.py

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -2,48 +2,21 @@
 import streamlit as st
 
 def crear_botones():
-    col1, col2, col3, col4 = st.columns(4)
+    # Usamos st.columns para crear un diseño con espacios alrededor de los botones para centrarlos
+    espacio_izq, col1, col2, col3, col4, espacio_der = st.columns([1,2,2,2,2,1])
 
-    # CSS para usar imágenes como fondo de botones
-    button_style = """
-    <style>
-        .btn {
-            border: none;
-            color: transparent;
-            background-color: transparent;
-            padding: 0px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            margin: 4px 2px;
-            cursor: pointer;
-            width: 200px;
-            height: 200px;
-        }
-        .btn1 {background-image: url('sources/aviones.jpg'); background-size: cover;}
-        .btn2 {background-image: url('sources/aeropuertos.jpg'); background-size: cover;}
-        .btn3 {background-image: url('sources/aerolineas.jpg'); background-size: cover;}
-        .btn4 {background-image: url('sources/datos.jpg'); background-size: cover;}
-    </style>
-    """
-
-    # Aplica el estilo CSS
-    st.markdown(button_style, unsafe_allow_html=True)
-
-    # Botones de Streamlit para gestionar interacciones
     with col1:
-        if st.button("", key="vuelos_usa_btn", **{"class": "btn btn1"}):
+        if st.button("Ver vuelos en USA", key="vuelos_usa_btn"):
             st.session_state.subpagina = "vuelos_usa"
 
     with col2:
-        if st.button("", key="aeropuertos_btn", **{"class": "btn btn2"}):
+        if st.button("Ver aeropuertos de USA", key="aeropuertos_btn"):
             st.session_state.subpagina = "aeropuertos"
 
     with col3:
-        if st.button("", key="aerolineas_btn", **{"class": "btn btn3"}):
+        if st.button("Ver aerolíneas de USA", key="aerolineas_btn"):
             st.session_state.subpagina = "aerolineas"
 
     with col4:
-        if st.button("", key="datos_btn", **{"class": "btn btn4"}):
+        if st.button("Ver datos de vuelos USA", key="datos_btn"):
             st.session_state.subpagina = "datos"
