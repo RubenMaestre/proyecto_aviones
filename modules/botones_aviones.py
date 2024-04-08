1d2f949b224b3cad175d176edaadd92adc0f829b
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 17:53:15 +0200

    Update botones_aviones.py

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -6,20 +6,28 @@ def crear_botones():
 
     with col1:
         st.image("sources/aviones.jpg", width=200)
-        if st.button('Ver vuelos en USA', key='btn_vuelos_usa'):
-            st.session_state.subpagina = 'vuelos_usa'
+        col1_1, col1_2, col1_3 = col1.columns([1,2,1])  # Ajusta las proporciones según sea necesario
+        with col1_2:
+            if st.button('Ver vuelos en USA', key='btn_vuelos_usa'):
+                st.session_state.subpagina = 'vuelos_usa'
 
     with col2:
         st.image("sources/aeropuertos.jpg", width=200)
-        if st.button('Ver aeropuertos de USA', key='btn_aeropuertos'):
-            st.session_state.subpagina = 'aeropuertos'
+        col2_1, col2_2, col2_3 = col2.columns([1,2,1])  # Ajusta las proporciones según sea necesario
+        with col2_2:
+            if st.button('Ver aeropuertos de USA', key='btn_aeropuertos'):
+                st.session_state.subpagina = 'aeropuertos'
 
     with col3:
         st.image("sources/aerolineas.jpg", width=200)
-        if st.button('Ver aerolíneas de USA', key='btn_aerolineas'):
-            st.session_state.subpagina = 'aerolineas'
+        col3_1, col3_2, col3_3 = col3.columns([1,2,1])  # Ajusta las proporciones según sea necesario
+        with col3_2:
+            if st.button('Ver aerolíneas de USA', key='btn_aerolineas'):
+                st.session_state.subpagina = 'aerolineas'
 
     with col4:
         st.image("sources/datos.jpg", width=200)
-        if st.button('Ver datos de vuelos USA', key='btn_datos'):
-            st.session_state.subpagina = 'datos'
+        col4_1, col4_2, col4_3 = col4.columns([1,2,1])  # Ajusta las proporciones según sea necesario
+        with col4_2:
+            if st.button('Ver datos de vuelos USA', key='btn_datos'):
+                st.session_state.subpagina = 'datos'
