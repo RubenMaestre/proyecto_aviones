ed4c8630966c76c913347e83ab311427ac5a4edd
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 18:16:23 +0200

    Update botones_aviones.py

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -17,8 +17,8 @@ def crear_botones():
     """
 
     def markdown_image(image_path, key):
-        # La función JavaScript aquí establece un valor en localStorage y recarga la página.
-        # En el servidor de Streamlit, necesitas verificar este valor en localStorage para determinar qué acción tomar.
+        # La función JavaScript aquí intenta establecer un valor en localStorage y recargar la página.
+        # Sin embargo, esta acción puede no funcionar como se espera en el entorno de Streamlit Sharing.
         return st.markdown(f"""
         <a href='javascript:void(0);'>
             <img class='clickable-image' src='{image_path}' onclick='if(typeof(Storage) !== "undefined") {{localStorage.setItem("{key}", "true"); location.reload(); }}' width='200'/>
@@ -31,20 +31,18 @@ def crear_botones():
             markdown_image("sources/aviones.jpg", 'btn_vuelos_usa')
 
     with col2:
-        col2_1, col2_2, col2_3 = col1.columns([1,4,1])
+        col2_1, col2_2, col2_3 = col2.columns([1,4,1])  # Corregido para usar col2
         with col2_2:
-            markdown_image("sources/aerolineas.jpg", 'btn_aeropuertos')
+            markdown_image("sources/aeropuertos.jpg", 'btn_aeropuertos')  # Asegúrate de que la imagen exista
 
     with col3:
-        col3_1, col3_2, col3_3 = col1.columns([1,4,1])
+        col3_1, col3_2, col3_3 = col3.columns([1,4,1])  # Corregido para usar col3
         with col3_2:
             markdown_image("sources/aerolineas.jpg", 'btn_aerolineas')
 
     with col4:
-        col4_1, col4_2, col4_3 = col1.columns([1,4,1])
+        col4_1, col4_2, col4_3 = col4.columns([1,4,1])  # Corregido para usar col4
         with col4_2:
             markdown_image("sources/datos.jpg", 'btn_datos')
 
-    # Repite para col2, col3, col4 con sus respectivas imágenes y claves.
-    
     st.markdown(image_style, unsafe_allow_html=True)
