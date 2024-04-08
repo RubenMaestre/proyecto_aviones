d318771ec4d628b1b770a69a49835e7f96d4d404
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 10:33:01 +0200

    Update aviones.py

--- paginas/aviones.py
+++ paginas/aviones.py
@@ -14,28 +14,28 @@ def display():
     # Crear columnas para los botones
     col1, col2, col3, col4 = st.columns(4)
 
-    # Botón para volver a la página de aviones (vuelos en USA)
+    # Botón de imagen para volver a la página de aviones (vuelos en USA)
     with col1:
-        if st.button("", image="sources/aviones.jpg"):
-            aviones.display()  # Asumiendo que tienes una función display en aviones.py
+        if st.image("sources/aviones.jpg", width=250, on_click=aviones.display):
+            pass  # La función aviones.display se llama cuando se hace clic en la imagen
 
-    # Botón para ir a la página de aeropuertos
+    # Botón de imagen para ir a la página de aeropuertos
     with col2:
-        if st.button("", image="sources/aeropuertos.jpg"):
-            aeropuertos.display()  # Asumiendo que tienes una función display en aeropuertos.py dentro de paginas/vuelos/
+        if st.image("sources/aeropuertos.jpg", width=250, on_click=aeropuertos.display):
+            pass  # La función aeropuertos.display se llama cuando se hace clic en la imagen
 
-    # Botón para ir a la página de aerolíneas
+    # Botón de imagen para ir a la página de aerolíneas
     with col3:
-        if st.button("", image="sources/aerolineas.jpg"):
-            aerolineas.display()  # Asumiendo que tienes una función display en aerolineas.py dentro de paginas/vuelos/
+        if st.image("sources/aerolineas.jpg", width=250, on_click=aerolineas.display):
+            pass  # La función aerolineas.display se llama cuando se hace clic en la imagen
 
-    # Botón para ir a la página de datos
+    # Botón de imagen para ir a la página de datos
     with col4:
-        if st.button("", image="sources/datos.jpg"):
-            datos.display()  # Asumiendo que tienes una función display en datos.py
+        if st.image("sources/datos.jpg", width=250, on_click=datos.display):
+            pass  # La función datos.display se llama cuando se hace clic en la imagen
 
     # Crear el mapa base con Folium
-    mapa = folium.Map(location=[38.2699, 30.7126], zoom_start=2)  # Punto centrado en Elche, la idea es que se vea todo el mundo desde punto vista Europa
+    mapa = folium.Map(location=[38.2699, 30.7126], zoom_start=2)
 
     # Añadir marcadores para cada aeropuerto
     for index, row in df_aeropuertos_unicos.iterrows():
@@ -43,4 +43,3 @@ def display():
 
     # Mostrar el mapa
     st_folium(mapa, width=1280, height=720, key="unique_map_key")
-
