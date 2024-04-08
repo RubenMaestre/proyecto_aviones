8998d606e71e1d46854b1764692fd678ca7b3ae3
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 10:18:16 +0200

    Update create_sidebar.py

--- modules/create_sidebar.py
+++ modules/create_sidebar.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from paginas import inicio, datos, eda, aviones, modelo, sobre_nosotros, sobre_proyecto
-from paginas.vuelos import aerolineas, aeropuertos, datos
 
 def create_sidebar():
     # Añadir texto personalizado en el sidebar con markdown y HTML
@@ -17,32 +16,18 @@ def create_sidebar():
     # Crear el menú de opciones en el sidebar con option_menu
     with st.sidebar:
         selected = option_menu("Menú", ["Inicio", "Datos", "EDA", "Vuelos en USA", "Modelo machine learning", "Sobre el proyecto", "Sobre nosotros"],
-            icons=["house", "disk", "bar-chart-line", "airplane", "cpu", "book", "people"],
+            icons=["house", "database", "bar-chart-line", "airplane", "cpu", "book", "people"],
             menu_icon="cast", default_index=0, orientation="vertical")
 
     # Llama a la función de la página correspondiente en función de la selección
     if selected == "Inicio":
         inicio.display()
-    elif selected == "Los datos":
+    elif selected == "Datos":
         datos.display()
     elif selected == "EDA":
         eda.display()
     elif selected == "Vuelos en USA":
-        aviones.display()  # Mostrar siempre el contenido principal de "Vuelos en USA"
-        
-        # Definir el submenú en la barra lateral
-        st.sidebar.markdown("### Submenú de Vuelos en USA")  # Título del submenú
-        submenu_options = ["Info Aerolíneas", "Info Aeropuertos", "Datos Curiosos"]
-        submenu_selected = st.sidebar.radio("Seleccione una opción:", submenu_options, key="submenu_vuelos_usa")
-
-        # Manejar la selección del submenú
-        if submenu_selected == "Info Aerolíneas":
-            aerolineas.display()
-        elif submenu_selected == "Info Aeropuertos":
-            aeropuertos.display()
-        elif submenu_selected == "Datos Curiosos":
-            datos.display()
-
+        aviones.display()
     elif selected == "Modelo machine learning":
         modelo.display()
     elif selected == "Sobre el proyecto":
