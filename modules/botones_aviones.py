40cbab8262ac49611598acb5cff21eb28c83e6b7
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 18:31:33 +0200

    Update botones_aviones.py

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -4,22 +4,20 @@ import streamlit as st
 def crear_botones():
     col1, col2, col3, col4 = st.columns(4)
 
-    def on_click(key):
-        st.session_state['navegar_a'] = key
+    # Define el enlace de cada imagen. En este caso, estamos simulando una acción de "navegación"
+    # al establecer una clave en st.session_state. La clave debería manejarse en tu app principal.
+    def markdown_image(image_path, key, caption=""):
+        return st.markdown(f"<div style='text-align: center;'><a href='javascript:void(0);' onclick='localStorage.setItem(\"{key}\", \"true\");'><img src='{image_path}' class='img-fluid' width='200'><p>{caption}</p></a></div>", unsafe_allow_html=True)
 
     with col1:
-        if st.image("sources/aviones.jpg", width=200, caption="Ver vuelos en USA", on_click=lambda: on_click('vuelos_usa')):
-            pass
+        markdown_image("sources/aviones.jpg", 'vuelos_usa', "Ver vuelos en USA")
 
     with col2:
-        if st.image("sources/aeropuertos.jpg", width=200, caption="Ver aeropuertos de USA", on_click=lambda: on_click('aeropuertos')):
-            pass
+        markdown_image("sources/aeropuertos.jpg", 'aeropuertos', "Ver aeropuertos de USA")
 
     with col3:
-        if st.image("sources/aerolineas.jpg", width=200, caption="Ver aerolíneas de USA", on_click=lambda: on_click('aerolineas')):
-            pass
+        markdown_image("sources/aerolineas.jpg", 'aerolineas', "Ver aerolíneas de USA")
 
     with col4:
-        if st.image("sources/datos.jpg", width=200, caption="Ver datos de vuelos USA", on_click=lambda: on_click('datos')):
-            pass
+        markdown_image("sources/datos.jpg", 'datos', "Ver datos de vuelos USA")
 
