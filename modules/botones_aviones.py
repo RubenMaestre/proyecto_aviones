1f71ad922eb9f836107c9262f230225c0940e5c9
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 10:50:59 +0200

    Update botones_aviones.py

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -5,31 +5,46 @@ from paginas.vuelos import aeropuertos, aerolineas, datos
 import streamlit as st
 
 def crear_botones(aviones, aeropuertos, aerolineas, datos):
-    st.title('Vuelos en USA')
+    boton_html_template = """
+    <style>
+    .boton {{
+        margin: 10px;
+        width: 240px;
+        height: 50px;
+        background-color: #FFFFFF;
+        color: #000000;
+        border: 3px solid #ADD8E6;
+        border-radius: 5px;
+        cursor: pointer;
+        outline: none;
+    }}
+    .boton:hover {{
+        border-color: #0000FF;
+    }}
+    </style>
+    <center><button class="boton" title="{}" onclick="handleClick('{}')">{}</button></center>
+    <script>
+    function handleClick(key) {{
+        window.parent.postMessage({{func: 'keyPress', key: key}}, '*');
+    }}
+    </script>
+    """
 
     # Crear columnas para los botones
     col1, col2, col3, col4 = st.columns(4)
 
-    # Mostrar imagen y botón personalizado para volver a la página de aviones (vuelos en USA)
     with col1:
         st.image("sources/aviones.jpg", width=250)
-        if st.button("Vuelos en USA", key="btn_aviones", help="Haz clic para ver Vuelos en USA"):
-            aviones.display()
+        st.markdown(boton_html_template.format("Haz clic para ver Vuelos en USA", "aviones", "Vuelos en USA"), unsafe_allow_html=True)
 
-    # Mostrar imagen y botón personalizado para ir a la página de aeropuertos
     with col2:
         st.image("sources/aeropuertos.jpg", width=250)
-        if st.button("Aeropuertos", key="btn_aeropuertos", help="Haz clic para ver Aeropuertos"):
-            aeropuertos.display()
+        st.markdown(boton_html_template.format("Haz clic para ver Aeropuertos", "aeropuertos", "Aeropuertos"), unsafe_allow_html=True)
 
-    # Mostrar imagen y botón personalizado para ir a la página de aerolíneas
     with col3:
         st.image("sources/aerolineas.jpg", width=250)
-        if st.button("Aerolíneas", key="btn_aerolineas", help="Haz clic para ver Aerolíneas"):
-            aerolineas.display()
+        st.markdown(boton_html_template.format("Haz clic para ver Aerolíneas", "aerolineas", "Aerolíneas"), unsafe_allow_html=True)
 
-    # Mostrar imagen y botón personalizado para ir a la página de datos
     with col4:
         st.image("sources/datos.jpg", width=250)
-        if st.button("Datos", key="btn_datos", help="Haz clic para ver Datos"):
-            datos.display()
+        st.markdown(boton_html_template.format("Haz clic para ver Datos", "datos", "Datos"), unsafe_allow_html=True)
