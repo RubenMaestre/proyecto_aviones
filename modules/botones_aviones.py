f7bda191d276410821b83d6f80b507c87a323356
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 10:42:40 +0200

    Update botones_aviones.py

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -2,34 +2,32 @@
 
 import streamlit as st
 
-def crear_botones():
+def crear_botones(aviones, aeropuertos, aerolineas, datos):
+    st.title('Vuelos en USA')
+
     # Crear columnas para los botones
     col1, col2, col3, col4 = st.columns(4)
 
-    # Botón para "Vuelos en USA"
+    # Mostrar imagen y botón personalizado para volver a la página de aviones (vuelos en USA)
     with col1:
-        st.image("sources/aviones.jpg", width=250, caption="Vuelos en USA")
-        if st.button("Ver", key="btn_aviones"):
-            # La acción deseada cuando se hace clic en el botón
-            pass
+        st.image("sources/aviones.jpg", width=250)
+        if st.button("Vuelos en USA", key="btn_aviones", help="Haz clic para ver Vuelos en USA"):
+            aviones.display()
 
-    # Botón para "Aeropuertos"
+    # Mostrar imagen y botón personalizado para ir a la página de aeropuertos
     with col2:
-        st.image("sources/aeropuertos.jpg", width=250, caption="Aeropuertos")
-        if st.button("Ver", key="btn_aeropuertos"):
-            # La acción deseada cuando se hace clic en el botón
-            pass
+        st.image("sources/aeropuertos.jpg", width=250)
+        if st.button("Aeropuertos", key="btn_aeropuertos", help="Haz clic para ver Aeropuertos"):
+            aeropuertos.display()
 
-    # Botón para "Aerolíneas"
+    # Mostrar imagen y botón personalizado para ir a la página de aerolíneas
     with col3:
-        st.image("sources/aerolineas.jpg", width=250, caption="Aerolíneas")
-        if st.button("Ver", key="btn_aerolineas"):
-            # La acción deseada cuando se hace clic en el botón
-            pass
+        st.image("sources/aerolineas.jpg", width=250)
+        if st.button("Aerolíneas", key="btn_aerolineas", help="Haz clic para ver Aerolíneas"):
+            aerolineas.display()
 
-    # Botón para "Datos"
+    # Mostrar imagen y botón personalizado para ir a la página de datos
     with col4:
-        st.image("sources/datos.jpg", width=250, caption="Datos")
-        if st.button("Ver", key="btn_datos"):
-            # La acción deseada cuando se hace clic en el botón
-            pass
+        st.image("sources/datos.jpg", width=250)
+        if st.button("Datos", key="btn_datos", help="Haz clic para ver Datos"):
+            datos.display()
