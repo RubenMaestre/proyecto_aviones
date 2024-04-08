923eb45f9dd237fc40efd6f8facbe6bb223ecdd6
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 17:57:10 +0200

    Update botones_aviones.py

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -5,12 +5,12 @@ def crear_botones():
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
-        col1_1, col1_2, col1_3 = col1.columns([1,2,1])  # Ajusta las proporciones según sea necesario
-        st.image("sources/aviones.jpg", width=200)
-        with col1_2:
+        col1_1, col1_2, col1_3 = col1.columns([1,6,1])  # Ajusta las proporciones para centrar más
+        with col1_2:  # Usa esta subcolumna para colocar tanto la imagen como el botón
+            st.image("sources/aviones.jpg", width=200)
             if st.button('Ver vuelos en USA', key='btn_vuelos_usa'):
                 st.session_state.subpagina = 'vuelos_usa'
-
+  
     with col2:
         st.image("sources/aeropuertos.jpg", width=200)
         col2_1, col2_2, col2_3 = col2.columns([1,2,1])  # Ajusta las proporciones según sea necesario
