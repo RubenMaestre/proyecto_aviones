0b44ed11ce1dd4e16fbbf0e6a465e152f9668f32
RubenMaestre <formacion@rubenmaestre.com>
2024-04-08 11:17:12 +0200

    actualiza

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -1,33 +1,21 @@
 # modules/botones_aviones.py
-from paginas import aviones
-from paginas.vuelos import aeropuertos, aerolineas, datos
-
 import streamlit as st
 
-def crear_botones():
-    # Definición de las acciones al hacer clic en cada imagen
-    def navegar_a_aviones():
-        st.session_state.pagina_actual = 'aviones'
-    
-    def navegar_a_aeropuertos():
-        st.session_state.pagina_actual = 'aeropuertos'
-    
-    def navegar_a_aerolineas():
-        st.session_state.pagina_actual = 'aerolineas'
-    
-    def navegar_a_datos():
-        st.session_state.pagina_actual = 'datos'
-    
+def crear_botones(navegar_a):
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
-        st.image("sources/aviones.jpg", width=250, on_click=navegar_a_aviones)
+        if st.image("sources/aviones.jpg", width=250, on_click=lambda: navegar_a('aviones')):
+            pass
 
     with col2:
-        st.image("sources/aeropuertos.jpg", width=250, on_click=navegar_a_aeropuertos)
+        if st.image("sources/aeropuertos.jpg", width=250, on_click=lambda: navegar_a('aeropuertos')):
+            pass
 
     with col3:
-        st.image("sources/aerolineas.jpg", width=250, on_click=navegar_a_aerolineas)
+        if st.image("sources/aerolineas.jpg", width=250, on_click=lambda: navegar_a('aerolineas')):
+            pass
 
     with col4:
-        st.image("sources/datos.jpg", width=250, on_click=navegar_a_datos)
+        if st.image("sources/datos.jpg", width=250, on_click=lambda: navegar_a('datos')):
+            pass
--- paginas/aviones.py
+++ paginas/aviones.py
@@ -3,9 +3,7 @@ import streamlit as st
 import pandas as pd
 from streamlit_folium import st_folium
 import folium
-from paginas import aviones
 from paginas.vuelos import aerolineas, aeropuertos, datos
-from modules.botones_aviones import crear_botones
 
 # Para cargar desde CSV
 df_aeropuertos_unicos = pd.read_pickle('data/aeropuertos_unicos.pkl')
@@ -13,29 +11,43 @@ df_aeropuertos_unicos = pd.read_pickle('data/aeropuertos_unicos.pkl')
 def display():
     st.title('Vuelos en USA')
 
-    # Inicialización de la variable de estado si aún no existe
-    if 'pagina_actual' not in st.session_state:
-        st.session_state.pagina_actual = 'inicio'
+    # Define las columnas para las imágenes/botones
+    col1, col2, col3, col4 = st.columns(4)
 
-    # Llamada a crear_botones en cada página o en un sidebar común
-    crear_botones()
+    with col1:
+        st.image("sources/aviones.jpg", width=250)
+        if st.button('Ver Aviones', key='btn_aviones'):
+            st.session_state.subpagina = 'aviones'
 
-    # Lógica para determinar qué página mostrar
-    if st.session_state.pagina_actual == 'aviones':
-        aviones.display()
-    elif st.session_state.pagina_actual == 'aeropuertos':
-        aeropuertos.display()
-    elif st.session_state.pagina_actual == 'aerolineas':
-        aerolineas.display()
-    elif st.session_state.pagina_actual == 'datos':
-        datos.display()
+    with col2:
+        st.image("sources/aeropuertos.jpg", width=250)
+        if st.button('Ver Aeropuertos', key='btn_aeropuertos'):
+            st.session_state.subpagina = 'aeropuertos'
 
-    # Crear el mapa base con Folium
+    with col3:
+        st.image("sources/aerolineas.jpg", width=250)
+        if st.button('Ver Aerolíneas', key='btn_aerolineas'):
+            st.session_state.subpagina = 'aerolineas'
+
+    with col4:
+        st.image("sources/datos.jpg", width=250)
+        if st.button('Ver Datos', key='btn_datos'):
+            st.session_state.subpagina = 'datos'
+
+    # Navegación interna basada en el estado de la sesión
+    if 'subpagina' in st.session_state:
+        if st.session_state.subpagina == 'aviones':
+            # Contenido específico de 'aviones'
+            pass  # Aquí puedes colocar contenido o llamadas a funciones específicas para 'aviones'
+        elif st.session_state.subpagina == 'aeropuertos':
+            aeropuertos.display()
+        elif st.session_state.subpagina == 'aerolineas':
+            aerolineas.display()
+        elif st.session_state.subpagina == 'datos':
+            datos.display()
+
+    # Mostrar el mapa base con Folium (esto siempre se muestra, independientemente de la subpágina)
     mapa = folium.Map(location=[38.2699, 30.7126], zoom_start=2)
-
-    # Añadir marcadores para cada aeropuerto
     for index, row in df_aeropuertos_unicos.iterrows():
         folium.Marker([row['latitude'], row['longitude']], popup=row['nombre_aeropuerto']).add_to(mapa)
-
-    # Mostrar el mapa
     st_folium(mapa, width=1280, height=720, key="unique_map_key")
