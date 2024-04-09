326632fbe4aa4d4200090b24c056faa96137eeac
RubenMaestre <formacion@rubenmaestre.com>
2024-04-09 10:41:40 +0200

    Update botones_aviones.py

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def crear_botones():
     # Usamos st.columns para crear un diseño con espacios alrededor de los botones para centrarlos
-    espacio_izq, col1, col2, col3, col4, espacio_der = st.columns([1,2,2,2,2,1])
+    col1, col2, col3, col4 = st.columns([1,1,1,1])
 
     with col1:
         if st.button("Información vuelos en USA", key="vuelos_usa_btn"):
