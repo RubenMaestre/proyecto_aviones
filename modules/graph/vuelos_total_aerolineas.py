eb70ce002f17b830ddb8c3493f25db43a8dacd7f
RubenMaestre <formacion@rubenmaestre.com>
2024-04-09 12:48:42 +0200

    Update vuelos_total_aerolineas.py

--- modules/graph/vuelos_total_aerolineas.py
+++ modules/graph/vuelos_total_aerolineas.py
@@ -11,8 +11,7 @@ def graficar_vuelos_por_aerolinea(df):
                  y='Número total de vuelos',
                  opacity=0.8,
                  title="Cantidad de vuelos nacionales por compañía aérea en Estados Unidos",
-                 color='Aerolineas',
-                 category_orders={"aerolinea": ["American Airlines"]})  # Ordena para que American Airlines aparezca primero
+                 color='aerolinea')  # Asegúrate de que 'color' se refiera a una columna existente o elimínalo si no es necesario
 
     # Ajustar el tamaño de la gráfica y actualizar otros aspectos del layout
     fig.update_layout(
@@ -20,15 +19,9 @@ def graficar_vuelos_por_aerolinea(df):
         xaxis_title='Compañías aéreas',
         yaxis_title='Cantidad total de vuelos',
         xaxis={'categoryorder': 'total descending'},
-        width=1280,
-        height=720
+        width=1280,  # Ancho personalizado
+        height=720   # Altura personalizada, ajusta según necesidad
     )
-
-    # Actualizar la leyenda para mostrar solo American Airlines
-    fig.for_each_trace(lambda t: t.update(showlegend=True if t.name == "American Airlines" else False))
-
-    # Texto instructivo sobre la interacción con la gráfica
-    st.markdown("Haz click en la leyenda para filtrar y ver la evolución de vuelos de una aerolínea específica.")
-
+    
     # Mostrar la figura en la aplicación Streamlit
     st.plotly_chart(fig)
