f719af1a4323242028564ea33836837e29aa2315
RubenMaestre <formacion@rubenmaestre.com>
2024-04-09 11:16:17 +0200

    actualiza

--- paginas/anual/dec_2021.py
+++ paginas/anual/dec_2021.py
@@ -1,5 +1,12 @@
 import streamlit as st
+from modules.carga_dec_2021 import cargar_unir_2021  # Asegúrate de que la ruta y el nombre de la función sean correctos
 
 def display():
     st.title('Datos diciembre 2021')
-    # Aquí iría el resto de tu contenido de la página de inicio
+    
+    # Llama a la función para cargar el DataFrame de 2021
+    df_dec_2021 = cargar_unir_2021()
+
+    # Ahora puedes usar df_dec_2021 para cualquier operación subsiguiente, como visualización, análisis, etc.
+    # Por ejemplo:
+    st.write(df_dec_2021.head())  # Muestra las primeras filas del DataFrame para confirmar que se ha cargado correctamente
