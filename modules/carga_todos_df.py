c83cd4ff93310dceb178a5f995ad0ee8c4dc04db
RubenMaestre <formacion@rubenmaestre.com>
2024-04-09 11:22:32 +0200

    actualiza

--- modules/carga_todos_df.py
+++ modules/carga_todos_df.py
@@ -1,13 +1,25 @@
-from .carga_dec_2021 import cargar_unir_2021  # Asegúrate de que el nombre de la función sea correcto
+import pandas as pd
+from .carga_dec_2021 import cargar_unir_2021
 from .carga_dec_2022 import cargar_unir_2022
 from .carga_dec_2023 import cargar_unir_2023
-import pandas as pd
 
 def cargar_todos_df():
-    # Cargar cada DataFrame de año
-    df_dec_2021 = cargar_unir_2021()
-    df_dec_2022 = cargar_unir_2022()
-    df_dec_2023 = cargar_unir_2023()
+    # Intentar cargar cada DataFrame de año, ya sea desde el estado de la aplicación o directamente desde los archivos pickle
+    try:
+        # Suponiendo que puedes verificar si ya están cargados en el estado de la aplicación (ajusta según tu implementación)
+        df_dec_2021 = st.session_state.df_dec_2021
+    except AttributeError:
+        df_dec_2021 = cargar_unir_2021()
+
+    try:
+        df_dec_2022 = st.session_state.df_dec_2022
+    except AttributeError:
+        df_dec_2022 = cargar_unir_2022()
+
+    try:
+        df_dec_2023 = st.session_state.df_dec_2023
+    except AttributeError:
+        df_dec_2023 = cargar_unir_2023()
 
     # Unir todos los DataFrames en uno solo
     df_todos = pd.concat([df_dec_2021, df_dec_2022, df_dec_2023], ignore_index=True)
--- paginas/anual/todos.py
+++ paginas/anual/todos.py
@@ -1,5 +1,14 @@
 import streamlit as st
+from modules.carga_todos_df import cargar_todos_df  # Asegúrate de que la ruta y el nombre de la función sean correctos
 
 def display():
     st.title('Todos los datos juntos')
-    # Aquí iría el resto de tu contenido de la página de inicio
+    
+    # Llama a la función para cargar y unir todos los DataFrames
+    df_todos = cargar_todos_df()
+
+    # Ahora puedes usar df_todos para cualquier operación subsiguiente, como visualización, análisis, etc.
+    # Por ejemplo:
+    st.write(df_todos.head())  # Muestra las primeras filas del DataFrame para confirmar que se ha cargado correctamente
+
+    # Aquí puedes añadir más lógica para trabajar con df_todos, como visualizaciones o análisis adicionales
