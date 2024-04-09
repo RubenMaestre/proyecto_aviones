0dca515028e63a00c7b7cb291f0be3cf7c987be7
RubenMaestre <formacion@rubenmaestre.com>
2024-04-09 11:46:04 +0200

    Update datos_df_cargados.py

--- modules/datos_df_cargados.py
+++ modules/datos_df_cargados.py
@@ -3,18 +3,15 @@ import locale
 from babel.dates import format_date
 
 def mostrar_estadisticas_df(df, columna_fecha):
-    # Intentar establecer el locale a español
     try:
         locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
     except locale.Error:
         pass  # En caso de que el locale 'es_ES.UTF-8' no esté disponible
 
     total_datos = len(df)
-    
-    # Usar babel para formatear las fechas si el locale no se ha podido cambiar
     fecha_min = df[columna_fecha].min()
     fecha_max = df[columna_fecha].max()
-    
+
     if 'locale' in locals() or 'locale' in globals():
         fecha_min_str = fecha_min.strftime('%d de %B de %Y')
         fecha_max_str = fecha_max.strftime('%d de %B de %Y')
@@ -22,22 +19,21 @@ def mostrar_estadisticas_df(df, columna_fecha):
         fecha_min_str = format_date(fecha_min, format='long', locale='es')
         fecha_max_str = format_date(fecha_max, format='long', locale='es')
 
-    # Usamos st.columns para crear una tabla con 4 columnas de diferentes tamaños
-    st.markdown("---")  # Línea horizontal como borde superior
+    st.markdown("---")
     col1, col2, col3, col4 = st.columns([1, 3, 1, 3])
 
     with col1:
-        st.write("Total datos:")
+        st.markdown(f"<h4 style='text-align: left;'>Total datos:</h4>", unsafe_allow_html=True)
 
     with col2:
-        # Usar markdown con HTML para personalizar el estilo
-        st.markdown(f"<h3 style='text-align: left; color: black;'>{total_datos:,}</h3>", unsafe_allow_html=True)
+        total_datos_str = f"{total_datos:,.0f}".replace(',', '.')
+        total_datos_str += " vuelos nacionales en USA"
+        st.markdown(f"<h4 style='text-align: left;'>{total_datos_str}</h4>", unsafe_allow_html=True)
 
     with col3:
-        st.write("Fecha datos:")
+        st.markdown(f"<h4 style='text-align: left;'>Fecha datos:</h4>", unsafe_allow_html=True)
 
     with col4:
-        st.write(f"Del {fecha_min_str} al {fecha_max_str}")
+        st.markdown(f"<h4 style='text-align: left;'>Del {fecha_min_str} al {fecha_max_str}</h4>", unsafe_allow_html=True)
 
-    st.markdown("---")  # Línea horizontal como borde inferior
-
+    st.markdown("---")
