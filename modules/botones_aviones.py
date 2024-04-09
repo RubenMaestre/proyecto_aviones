ce879ac2c50fed49dc1aaf2afd43cf88e9baae14
RubenMaestre <formacion@rubenmaestre.com>
2024-04-09 10:43:13 +0200

    Update botones_aviones.py

--- modules/botones_aviones.py
+++ modules/botones_aviones.py
@@ -6,17 +6,17 @@ def crear_botones():
     col1, col2, col3, col4 = st.columns([1,1,1,1])
 
     with col1:
-        if st.button("Información vuelos en USA", key="vuelos_usa_btn"):
+        if st.button("✈️ Información vuelos en USA", key="vuelos_usa_btn"):
             st.session_state.subpagina = "vuelos_usa"
 
     with col2:
-        if st.button("Ver aeropuertos de USA", key="aeropuertos_btn"):
+        if st.button("🛫 Ver aeropuertos de USA", key="aeropuertos_btn"):
             st.session_state.subpagina = "aeropuertos"
 
     with col3:
-        if st.button("Ver aerolíneas de USA", key="aerolineas_btn"):
+        if st.button("🏢 Ver aerolíneas de USA", key="aerolineas_btn"):
             st.session_state.subpagina = "aerolineas"
 
     with col4:
-        if st.button("Ver datos de vuelos USA", key="datos_btn"):
+        if st.button("📊 Ver datos de vuelos USA", key="datos_btn"):
             st.session_state.subpagina = "datos"
