defcbb7810e672d39179d4509877774d2128f4e1
RubenMaestre <formacion@rubenmaestre.com>
2024-04-09 13:25:26 +0200

    Update diagrama_salidas_llegadas.py

--- modules/graph/diagrama_salidas_llegadas.py
+++ modules/graph/diagrama_salidas_llegadas.py
@@ -12,11 +12,20 @@ def graficar_horas_vuelos(df):
     fig.add_trace(go.Box(x=df["hora_salida_minutos"], name="Salida"))
     fig.add_trace(go.Box(x=df["hora_llegada_minutos"], name="Llegada"))
 
+    # Definir las marcas personalizadas para el eje x
+    tickvals = [i * 60 for i in range(25)]  # Cada hora en minutos
+    ticktext = [f'{i}:00' for i in range(25)]  # Etiquetas de horas
+
     fig.update_layout(
         title="Diagrama de caja para las horas de salida y llegada de los vuelos",
         title_x=0.5,
-        xaxis_title="Minutos desde la medianoche",
+        xaxis_title="Hora del día",
         yaxis_title="Distribución de las horas",
+        xaxis=dict(
+            tickmode='array',
+            tickvals=tickvals,
+            ticktext=ticktext
+        ),
         width=1080,  # Ancho personalizado
         height=720   # Altura personalizada
     )
