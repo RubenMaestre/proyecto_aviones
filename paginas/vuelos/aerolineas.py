f6301302ed63354cbc738032619316488f796b12
RubenMaestre <formacion@rubenmaestre.com>
2024-04-11 17:16:15 +0200

    actualiza

--- paginas/vuelos/aerolineas.py
+++ paginas/vuelos/aerolineas.py
@@ -1,8 +1,13 @@
 import streamlit as st
 from modules.map.aerolineas_unicos import mostrar_aerolineas_unicos
+from modules.map.aerolineas_medias import aerolineas_medias
 
 def display():
     st.title('Aerolineas')
     # Aquí iría el resto de tu contenido de la página de inicio
+
+    aerolineas_medias
+    
+    st.markdown("---")
 
     mostrar_aerolineas_unicos()
