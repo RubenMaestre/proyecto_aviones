6578876fcba04917311e978c45a4e8e69d1aa8ef
RubenMaestre <formacion@rubenmaestre.com>
2024-04-11 15:26:32 +0200

    actualiza

---
+++ modules/map/aerolineas_unicos.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import streamlit as st
+
+def mostrar_aerolineas_unicas():
+    # Cargar los datos de aerolíneas
+    df_aerolineas_unicas = pd.read_pickle('data/aerolineas_unicas.pkl')
+
+    # Selector de Aerolínea
+    aerolineas = df_aerolineas_unicas['aerolinea'].tolist()
+    aerolinea_seleccionada = st.selectbox('Selecciona una aerolínea:', aerolineas)
+
+    # Información de la Aerolínea seleccionada
+    aerolinea_info = df_aerolineas_unicas[df_aerolineas_unicas['aerolinea'] == aerolinea_seleccionada].iloc[0]
+
+    # Mostrar tabla con información de la aerolínea
+    col1, col2 = st.columns([3, 2])  # Ajusta la proporción de las columnas según necesites
+
+    with col1:
+        st.markdown(f"### {aerolinea_info['aerolinea']}")
+        st.write(aerolinea_info['Resumen'])
+
+    with col2:
+        st.image(aerolinea_info['Imagen_URL'], width=400, caption=aerolinea_info['aerolinea'])
--- paginas/vuelos/aerolineas.py
+++ paginas/vuelos/aerolineas.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from modules.map.aeropuertos_unicos import mostrar_aerolineas_unicos
 
 def display():
     st.title('Aerolineas')
     # Aquí iría el resto de tu contenido de la página de inicio
+
+    mostrar_aerolineas_unicas()
