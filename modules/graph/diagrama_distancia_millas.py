85eeb9a590d680ab6e51488246be85a483bd4dab
RubenMaestre <formacion@rubenmaestre.com>
2024-04-11 11:34:29 +0200

    actualiza

--- modules/graph/diagrama_distancia_millas.py
+++ modules/graph/diagrama_distancia_millas.py
@@ -1,15 +1,22 @@
 import streamlit as st
-import plotly.express as px
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 def graficar_diagrama_distancia_millas(df):
-    fig = px.box(df, x='distancia_millas', notched=True, points="all")
-
-    fig.update_layout(
-        title='Diagrama de Caja de la Distancia en Millas',
-        xaxis_title='Distancia en Millas',
-        title_x=0.5,
-        width=1080
-    )
-
-    # Mostrar la figura en la aplicación Streamlit
-    st.plotly_chart(fig)
+    # Configurar el estilo de Seaborn para los gráficos
+    sns.set(style="whitegrid")
+    
+    # Crear la figura y el eje para el gráfico
+    fig, ax = plt.subplots()
+    
+    # Crear el diagrama de caja
+    sns.boxplot(x=df['distancia_millas'], notch=True, ax=ax)
+    
+    # Establecer el título y las etiquetas de los ejes
+    ax.set_title('Diagrama de Caja de la Distancia en Millas', fontsize=16)
+    ax.set_xlabel('Distancia en Millas', fontsize=14)
+    
+    # Ajustar las dimensiones de la figura si es necesario
+    fig.set_size_inches(15, 6)  # Tamaño en pulgadas (ancho, alto)
+    
+    # Mostrar el gráfico en Streamlit
+    st.pyplot(fig)
