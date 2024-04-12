af59acd719ac658cbd82fd2110fbb60008a99c41
RubenMaestre <formacion@rubenmaestre.com>
2024-04-12 16:08:21 +0200

    actualiza

--- modules/ml/ml_1.py
+++ modules/ml/ml_1.py
@@ -1,30 +1,19 @@
 # modules/ml/ml_1.py
 import streamlit as st
 import pandas as pd
-import pickle
 import numpy as np
+from joblib import load
 from modules.carga_todos_df import cargar_todos_df
 
-# Importaciones de Scikit-learn y Imbalanced-learn
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import jaccard_score, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
-from imblearn.over_sampling import SMOTE
-from imblearn.under_sampling import RandomUnderSampler
-from imblearn.pipeline import Pipeline
-
-
 def load_model(path):
-    with open(path, 'rb') as file:
-        model = pickle.load(file)
+    model = load(path)
     return model
 
 def display_ml_page():
     st.title('Predicción de Retrasos de Vuelos')
 
     # Cargar modelo
-    model = load_model('data/modelo_entrenado.pkl')
+    model = load_model('data/modelo_entrenado.joblib')
 
     # Cargar datos y asegurar transformación adecuada
     df = cargar_todos_df()
@@ -39,8 +28,6 @@ def display_ml_page():
     if st.button('Predecir Retraso'):
         # Aquí necesitas asegurarte de que las entradas estén en el formato adecuado
         features = np.array([[ciudad_origen, aeropuerto_origen, ciudad_destino, aerolinea]])
-        # Transformar las características aquí si es necesario antes de la predicción, por ejemplo con un encoder
-        # Si usaste un encoder durante el entrenamiento, deberías cargarlo y usarlo aquí para transformar 'features'
         prediction = model.predict(features)
 
         if prediction[0] == 1:
