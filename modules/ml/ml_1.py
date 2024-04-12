0af77b0dbf4a97dea399c865dd2c66e19cfc82e3
RubenMaestre <formacion@rubenmaestre.com>
2024-04-12 16:30:21 +0200

    Update ml_1.py

--- modules/ml/ml_1.py
+++ modules/ml/ml_1.py
@@ -13,21 +13,22 @@ def display_ml_page():
     st.title('Predicción de Retrasos de Vuelos')
 
     # Cargar modelo
-    model = load_model('data/modelo_entrenado.joblib')
+    model = load_model('data/model_nuevo.joblib')
 
     # Cargar datos y asegurar transformación adecuada
     df = cargar_todos_df()
 
+    # Seleccionar opciones para la predicción
     ciudad_origen = st.selectbox('Selecciona la ciudad de origen:', options=df['ciudad_origen'].unique())
-    aeropuertos_disponibles = df[df['ciudad_origen'] == ciudad_origen]['aeropuerto_origen'].unique()
-    aeropuerto_origen = st.selectbox('Selecciona el aeropuerto de origen:', options=aeropuertos_disponibles)
+    aeropuerto_origen = st.selectbox('Selecciona el aeropuerto de origen:', options=df[df['ciudad_origen'] == ciudad_origen]['aeropuerto_origen'].unique())
     ciudad_destino = st.selectbox('Selecciona la ciudad destino:', options=df['ciudad_destino'].unique())
-    aerolineas_disponibles = df[(df['ciudad_destino'] == ciudad_destino) & (df['ciudad_origen'] == ciudad_origen)]['aerolinea'].unique()
-    aerolinea = st.selectbox('Selecciona la aerolínea:', options=aerolineas_disponibles)
+    aerolinea = st.selectbox('Selecciona la aerolínea:', options=df[(df['ciudad_destino'] == ciudad_destino) & (df['ciudad_origen'] == ciudad_origen)]['aerolinea'].unique())
 
     if st.button('Predecir Retraso'):
-        # Aquí necesitas asegurarte de que las entradas estén en el formato adecuado
+        # Preparar features para la predicción, asegúrate de que están en el formato adecuado
         features = np.array([[ciudad_origen, aeropuerto_origen, ciudad_destino, aerolinea]])
+        # Si el modelo fue entrenado con datos codificados, necesitas aplicar la misma transformación aquí
+        # Transformar las características aquí si es necesario antes de la predicción
         prediction = model.predict(features)
 
         if prediction[0] == 1:
