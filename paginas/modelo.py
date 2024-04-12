2e95a0c0340fbf7647a39a7ce5d374af52b7a8c8
RubenMaestre <formacion@rubenmaestre.com>
2024-04-12 13:41:10 +0200

    Update modelo.py

--- paginas/modelo.py
+++ paginas/modelo.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from modules.ml.ml_1 import display_ml_page
 
 def display():
     st.title('Modelo machine learning')
     # Aquí iría el resto de tu contenido de la página de inicio
+
+    display_ml_page()
