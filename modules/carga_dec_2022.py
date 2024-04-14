c3a739821ae0a9852717af6da0f987cf2133196b
RubenMaestre <formacion@rubenmaestre.com>
2024-04-14 13:28:11 +0200

    actualiza

--- modules/carga_dec_2022.py
+++ modules/carga_dec_2022.py
@@ -3,14 +3,14 @@ import streamlit as st
 
 @st.cache_data
 def cargar_unir_2022():
-    # Clave 'df_dec_2023' en el estado de sesión para verificar su existencia
+    # Clave 'df_dec_2022' en el estado de sesión para verificar su existencia
     if 'df_dec_2022' not in st.session_state:
-        archivo_1 = 'data/pickles/df_2022_1.pkl'
-        archivo_2 = 'data/pickles/df_2022_2.pkl'
+        archivo_1 = 'data/parquet/df_2022_1.parquet'
+        archivo_2 = 'data/parquet/df_2022_2.parquet'
 
         # Cargar y unir los DataFrames solo si no existen en el estado de sesión
-        df_2022_1 = pd.read_pickle(archivo_1)
-        df_2022_2 = pd.read_pickle(archivo_2)
+        df_2022_1 = pd.read_parquet(archivo_1)
+        df_2022_2 = pd.read_parquet(archivo_2)
 
         # Guardar el DataFrame unido en el estado de sesión
         st.session_state.df_dec_2022 = pd.concat([df_2022_1, df_2022_2], ignore_index=True)
--- modules/carga_dec_2023.py
+++ modules/carga_dec_2023.py
@@ -5,12 +5,12 @@ import streamlit as st
 def cargar_unir_2023():
     # Clave 'df_dec_2023' en el estado de sesión para verificar su existencia
     if 'df_dec_2023' not in st.session_state:
-        archivo_1 = 'data/pickles/df_2023_1.pkl'
-        archivo_2 = 'data/pickles/df_2023_2.pkl'
+        archivo_1 = 'data/parquet/df_2023_1.parquet'
+        archivo_2 = 'data/parquet/df_2023_2.parquet'
 
         # Cargar y unir los DataFrames solo si no existen en el estado de sesión
-        df_2023_1 = pd.read_pickle(archivo_1)
-        df_2023_2 = pd.read_pickle(archivo_2)
+        df_2023_1 = pd.read_parquet(archivo_1)
+        df_2023_2 = pd.read_parquet(archivo_2)
 
         # Guardar el DataFrame unido en el estado de sesión
         st.session_state.df_dec_2023 = pd.concat([df_2023_1, df_2023_2], ignore_index=True)
