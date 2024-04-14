da82550d4962ca14da4ff8c375ce61c068f3a0aa
RubenMaestre <formacion@rubenmaestre.com>
2024-04-14 13:24:59 +0200

    actualiza

--- modules/carga_todos_df.py
+++ modules/carga_todos_df.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 @st.cache_data
 def cargar_todos_df():
-    # Define los años y la cantidad de archivos pickle por año
+    # Define los años y la cantidad de archivos Parquet por año
     anios = {
         '2023': 2,
         '2022': 2,
@@ -13,11 +13,11 @@ def cargar_todos_df():
     if 'df_todos' not in st.session_state:
         dataframes = []
         
-        # Iterar sobre cada año y cargar cada pickle correspondiente
+        # Iterar sobre cada año y cargar cada archivo Parquet correspondiente
         for anio, num_archivos in anios.items():
             for i in range(1, num_archivos + 1):
-                archivo = f'data/pickles/df_{anio}_{i}.pkl'
-                df_temp = pd.read_pickle(archivo)
+                archivo = f'data/parquet/df_{anio}_{i}.parquet'
+                df_temp = pd.read_parquet(archivo)
                 dataframes.append(df_temp)
         
         # Concatenar todos los DataFrames en uno solo
@@ -25,3 +25,4 @@ def cargar_todos_df():
 
     # Devolver el DataFrame desde el estado de sesión
     return st.session_state.df_todos
+
