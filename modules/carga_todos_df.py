61646b7e853177ff0da1922d373cc0504264b609
RubenMaestre <formacion@rubenmaestre.com>
2024-04-14 12:43:42 +0200

    Update carga_todos_df.py

--- modules/carga_todos_df.py
+++ modules/carga_todos_df.py
@@ -4,17 +4,11 @@ from .carga_dec_2021 import cargar_unir_2021
 from .carga_dec_2022 import cargar_unir_2022
 from .carga_dec_2023 import cargar_unir_2023
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data
 def cargar_todos_df():
-    # Verificar si el DataFrame combinado ya está en el estado de sesión
     if 'df_todos' not in st.session_state:
-        # Cargar cada uno de los DataFrames anuales usando las funciones respectivas
         df_dec_2021 = cargar_unir_2021()
         df_dec_2022 = cargar_unir_2022()
         df_dec_2023 = cargar_unir_2023()
-
-        # Concatenar los DataFrames cargados en uno solo
         st.session_state.df_todos = pd.concat([df_dec_2021, df_dec_2022, df_dec_2023], ignore_index=True)
-
-    # Devolver el DataFrame combinado desde el estado de sesión
     return st.session_state.df_todos
