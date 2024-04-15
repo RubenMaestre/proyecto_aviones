3f6f92a5ceb9f28fb86edbdcc53bcfd29e12d6fc
RubenMaestre <formacion@rubenmaestre.com>
2024-04-15 19:34:29 +0200

    Update vuelos_usa.py

--- paginas/vuelos/vuelos_usa.py
+++ paginas/vuelos/vuelos_usa.py
@@ -3,18 +3,13 @@ import pandas as pd
 from streamlit_folium import st_folium
 
 from modules.map.mapa_aeropuertos import mostrar_mapa_aeropuertos_globales
-from modules.map.estados_usa import mostrar_mapa_aeropuertos_usa
 from modules.map.selector_estado import mostrar_mapa_aeropuertos_por_estado
-from modules.map.selector_estado_interactivo import mostrar_mapa_aeropuertos_por_estado_interactivo
-
-
 
 # Para cargar desde CSV
 df_aeropuertos_unicos = pd.read_pickle('data/aeropuertos_unicos.pkl')
 
 def display():
     st.title('Mapas con todos los aeropuertos en Estados Unidos, incluídos territorios no incorporados')
-    # Aquí iría el resto de tu contenido de la página de inicio
     
     st.markdown("""
             #### El mapa tiene un zoom enorme para que pueda abarcar todos los aeropuertos que de alguna forma pertenecen a Estados Unidos. Puede hacer zoom para visualizar mejor por zonas.""")
@@ -25,12 +20,17 @@ def display():
 
     st.markdown("""
                 ### Selecciona un estado para ver los aeropuertos que contiene""")
-    st.write("En este mapa puede ver la cantidad de aeropuertos que hay por estado. Por ello le rogamos que seleccione uno de los 50 estados, el distrito federal, estados asociados o no incorporados.")
+                
+    # Cambiando st.write por st.markdown para mantener la consistencia en el formato de texto
+    st.markdown("En este mapa puede ver la cantidad de aeropuertos que hay por estado. Por ello le rogamos que seleccione uno de los 50 estados, el distrito federal, estados asociados o no incorporados.")
 
     mostrar_mapa_aeropuertos_por_estado()
 
     st.markdown("---")
+    
+    # Añadir más contenido o cerrar con otro markdown si es necesario
+    st.markdown("Continúe explorando para obtener más información sobre los aeropuertos en Estados Unidos.")
 
-
+display()
 
 
