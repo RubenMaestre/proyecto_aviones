c0c9792d94c10e1ee69cebc5db9ee08546ff7b1e
RubenMaestre <formacion@rubenmaestre.com>
2024-04-15 19:59:02 +0200

    Update aerolineas_medias.py

--- modules/map/aerolineas_medias.py
+++ modules/map/aerolineas_medias.py
@@ -26,25 +26,23 @@ def muestra_aerolineas_medias():
     ('Compañía aérea más antigua', f"{df_aerolineas_unicas.loc[oldest_idx, 'aerolinea']} (Fundada en {int(df_aerolineas_unicas.loc[oldest_idx, 'oldest'])})"),
     ('Flota más grande', f"{df_aerolineas_unicas.loc[largest_fleet_idx, 'aerolinea']} ({int(df_aerolineas_unicas.loc[largest_fleet_idx, 'fleet_size'])} aviones)"),
     ('Flota más pequeña', f"{df_aerolineas_unicas.loc[smallest_fleet_idx, 'aerolinea']} ({int(df_aerolineas_unicas.loc[smallest_fleet_idx, 'fleet_size'])} aviones)"),
-    ('Flota de mayor edad promedio', f"{df_aerolineas_unicas.loc[oldest_fleet_idx, 'aerolinea']} ({df_aerolineas_unicas.loc[oldest_fleet_idx, 'average_fleet_Age']:.2f} años)"),
-    ('Flota de menor edad promedio', f"{df_aerolineas_unicas.loc[youngest_fleet_idx, 'aerolinea']} ({df_aerolineas_unicas.loc[youngest_fleet_idx, 'average_fleet_Age']:.2f} años)"),
+    ('Flota de mayor edad de media', f"{df_aerolineas_unicas.loc[oldest_fleet_idx, 'aerolinea']} ({df_aerolineas_unicas.loc[oldest_fleet_idx, 'average_fleet_Age']:.2f} años)"),
+    ('Flota más joven de media', f"{df_aerolineas_unicas.loc[youngest_fleet_idx, 'aerolinea']} ({df_aerolineas_unicas.loc[youngest_fleet_idx, 'average_fleet_Age']:.2f} años)"),
     ('Media del tamaño de la flota', f"{df_aerolineas_unicas['fleet_size'].mean():.2f} aviones"),
-    ('Edad media de la flota', f"{df_aerolineas_unicas['average_fleet_Age'].mean():.2f} años"),
+    ('Edad media de la flota ', f"{df_aerolineas_unicas['average_fleet_Age'].mean():.2f} años"),
     ('Compañías con menos de 150 aviones', f"{(df_aerolineas_unicas['fleet_size'] < 150).sum()} compañías ({', '.join(df_aerolineas_unicas[df_aerolineas_unicas['fleet_size'] < 150]['aerolinea'].tolist())})"),
     ('Compañías con 150-500 aviones', f"{((df_aerolineas_unicas['fleet_size'] >= 150) & (df_aerolineas_unicas['fleet_size'] <= 500)).sum()} compañías ({', '.join(df_aerolineas_unicas[(df_aerolineas_unicas['fleet_size'] >= 150) & (df_aerolineas_unicas['fleet_size'] <= 500)]['aerolinea'].tolist())})"),
     ('Compañías con más de 500 aviones', f"{(df_aerolineas_unicas['fleet_size'] > 500).sum()} compañías ({', '.join(df_aerolineas_unicas[df_aerolineas_unicas['fleet_size'] > 500]['aerolinea'].tolist())})")
         ]
     
-    # Crear los cuadrados con la información
-    for i in range(0, len(datos), 2):
-        cols = st.columns(2)
-        for j in range(2):
+    # Crear 5 columnas en 2 filas
+    for i in range(0, len(datos), 5):
+        cols = st.columns(5)
+        for j in range(5):
             if i + j < len(datos):
                 with cols[j]:
-                    dato, descripcion = datos[i + j]
-                    st.markdown(f"#### {dato}")
-                    st.write(descripcion)
-                    st.markdown("---")  # Agregar una línea horizontal para separar visualmente los bloques
+                    st.markdown(f"<h6 style='text-align: center;'>{datos[i + j][0]}</h6>", unsafe_allow_html=True)
+                    st.write(datos[i + j][1])
 
 # Llamar a la función fuera de su definición
 muestra_aerolineas_medias()
