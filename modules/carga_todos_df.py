1df69172bd95641480e77a776a2864e3fd7e9479
RubenMaestre <formacion@rubenmaestre.com>
2024-04-15 09:33:19 +0200

    Update carga_todos_df.py

--- modules/carga_todos_df.py
+++ modules/carga_todos_df.py
@@ -1,16 +1,14 @@
 import pandas as pd
 import streamlit as st
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def cargar_todos_df():
-    # Ruta al archivo único Parquet
-    archivo = 'data/pickles/vuelos_limpio.parquet'
-    
+    # Verifica si 'df_todos' ya está en el estado de sesión
     if 'df_todos' not in st.session_state:
+        # Aquí, ajusta la ruta y el archivo según tus necesidades
+        archivo = 'data/pickles/vuelos_limpio.parquet'
         # Cargar el DataFrame desde el archivo Parquet
         st.session_state.df_todos = pd.read_parquet(archivo)
 
-    # Devolver el DataFrame desde el estado de sesión
     return st.session_state.df_todos
-
 
