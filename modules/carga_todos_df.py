a030371db6d2547692f3cb146afc786ead91dcb6
RubenMaestre <formacion@rubenmaestre.com>
2024-04-15 09:40:08 +0200

    Update carga_todos_df.py

--- modules/carga_todos_df.py
+++ modules/carga_todos_df.py
@@ -9,7 +9,7 @@ def cargar_todos_df():
     # Verificar si la clave ya está en el estado de sesión
     if clave not in st.session_state:
         # Ruta al archivo Parquet
-        archivo = 'C:/Users/34670/Desktop/python/Hack a boss/proyecto_3/streamlit/proyecto_aviones/data/pickles/vuelos_limpio.parquet'
+        archivo = 'data/pickles/vuelos_limpio.parquet'
         # Cargar el DataFrame desde el archivo Parquet y guardarlo en el estado de sesión
         st.session_state[clave] = pd.read_parquet(archivo)
     
