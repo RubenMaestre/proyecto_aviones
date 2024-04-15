76d81c1aaf947214021c27fb9c4521bd8b0fa129
RubenMaestre <formacion@rubenmaestre.com>
2024-04-15 18:46:51 +0200

    actualiza

--- paginas/eda.py
+++ paginas/eda.py
@@ -1,58 +1,32 @@
-# paginas/eda.py
+# paginas/eda_2.py
 import streamlit as st
-import plotly.express as px
-from modules.botones_eda import crear_botones_eda  # Importa la función de botones
-
-# Importa las funciones display de cada año y TODOS
-from paginas.anual.todos_1 import display as display_todos_1
-from paginas.anual.todos_2 import display as display_todos_2
-from paginas.anual.todos_3 import display as display_todos_3
-from paginas.anual.todos_4 import display as display_todos_4
-from paginas.anual.todos_5 import display as display_todos_5
-from paginas.anual.dec_2023 import display as display_2023
-from paginas.anual.dec_2022 import display as display_2022
-from paginas.anual.dec_2021 import display as display_2021
-from paginas.anual.dec_2023_2 import display as display_2023_2
-from paginas.anual.dec_2022_2 import display as display_2022_2
-from paginas.anual.dec_2021_2 import display as display_2021_2
+from modules.eda.botones_eda_2 import seleccionar_datos
+from modules.eda.botones_graficas import seleccionar_grafica
+from modules.eda.datos_df_cargados import mostrar_estadisticas_df
 
 def display():
-    st.title('Exploración de Datos de Vuelos')
+    st.markdown("<h2 style='text-align: center;'>Página de análisis exploratorio de datos (EDA)</h2>", unsafe_allow_html=True)
+    st.markdown("---")
+    if 'subpagina_eda' not in st.session_state:
+        st.markdown("<h4 style='text-align: center;'>En esta sección de nuestro proyecto, te ofrecemos la oportunidad de explorar un análisis detallado de los datos a través de diversos gráficos y visualizaciones. Este espacio está diseñado para que puedas entender mejor y analizar de forma intuitiva la información que hemos recopilado.</h4>", unsafe_allow_html=True)
+        st.image('sources/mapa_aviones_usa.png')
 
-    # Llama a la función para mostrar los botones de navegación
-    crear_botones_eda()
+    st.markdown("---")
 
-    # Muestra el texto centrado solo si no se ha seleccionado una subpágina
-    if 'subpagina_eda' not in st.session_state or st.session_state.subpagina_eda is None:
-        st.markdown("<h3 style='text-align: center;'>Seleccione un año para visualizar los datos</h3>", unsafe_allow_html=True)
-        st.image('sources/mapa_aviones_usa.png')  
-        
-    # Navegación interna basada en el estado de la sesión
-    if 'subpagina_eda' in st.session_state:
-        if st.session_state.subpagina_eda == '2023':
-            display_2023()
-        elif st.session_state.subpagina_eda == '2023_2':
-            display_2023_2()
-        elif st.session_state.subpagina_eda == '2022':
-            display_2022()
-        elif st.session_state.subpagina_eda == '2022_2':
-            display_2022_2()
-        elif st.session_state.subpagina_eda == '2021':
-            display_2021()
-        elif st.session_state.subpagina_eda == '2021_2':
-            display_2021_2()
-        elif st.session_state.subpagina_eda == 'todos_1':
-            display_todos_1()
-        elif st.session_state.subpagina_eda == 'todos_2':
-            display_todos_2()
-        elif st.session_state.subpagina_eda == 'todos_3':
-            display_todos_3()
-        elif st.session_state.subpagina_eda == 'todos_4':
-            display_todos_4()
-        elif st.session_state.subpagina_eda == 'todos_5':
-            display_todos_5()
+    col1, col2 = st.columns([1, 4])
+    with col1:
+        st.write("Puedes seleccionar el mes de diciembre de los años 2021, 2022 y 2023, o bien elegir todos los años para ver todos los datos juntos.")
+        df_seleccionado = seleccionar_datos()
+        if df_seleccionado is not None:
+            st.markdown("---")
+            mostrar_estadisticas_df(df_seleccionado, 'fecha')
+            st.markdown("---")
+            st.write("Una vez que has elegido el año, ahora puedes elegir qué tipo de gráfica ver. Para ayudarte hemos agrupado las gráficas por categorías.")
+            grafica_funcion = seleccionar_grafica()
 
-    
-# Llama a la función para mostrar la página
+    with col2:
+        if df_seleccionado is not None and grafica_funcion is not None:
+            grafica_funcion(df_seleccionado)
+
 display()
 
