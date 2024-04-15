6c0d7e85f9ffd71517972580913a3f2c2bd5dd67
RubenMaestre <formacion@rubenmaestre.com>
2024-04-15 16:09:23 +0200

    Update eda_2.py

--- paginas/eda_2.py
+++ paginas/eda_2.py
@@ -6,17 +6,22 @@ from modules.botones_graficas import seleccionar_y_mostrar_grafica
 def display():
     st.title('Exploración de Datos de Vuelos')
 
-    # Muestra el texto centrado solo si no se ha seleccionado una subpágina
+    # Muestra el texto centrado y la imagen si no se ha seleccionado una subpágina
     if 'subpagina_eda' not in st.session_state:
         st.markdown("<h3 style='text-align: center;'>Seleccione un año para visualizar los datos</h3>", unsafe_allow_html=True)
-        st.image('sources/mapa_aviones_usa.png')  
+        st.image('sources/mapa_aviones_usa.png')
+    
+    # Dividir la interfaz en dos columnas
+    col1, col2 = st.columns([1, 4])
 
-    # Seleccionar los datos según el año
-    df_seleccionado = seleccionar_datos()
+    with col1:
+        # Seleccionar los datos según el año desde el primer selector
+        df_seleccionado = seleccionar_datos()
 
-    # Seleccionar y mostrar la gráfica según el DataFrame cargado
-    if df_seleccionado is not None:
-        seleccionar_y_mostrar_grafica(df_seleccionado)
+    with col2:
+        # Si se ha seleccionado algún conjunto de datos, mostrar gráficas para ese conjunto
+        if df_seleccionado is not None:
+            seleccionar_y_mostrar_grafica(df_seleccionado)
 
 # Llama a la función para mostrar la página
 display()
