0323c6f0755fb86ed2320c7a8c0c14656af3c474
RubenMaestre <formacion@rubenmaestre.com>
2024-04-15 17:02:43 +0200

    Update eda_2.py

--- paginas/eda_2.py
+++ paginas/eda_2.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from modules.botones_eda_2 import seleccionar_datos
 from modules.botones_graficas import seleccionar_grafica
+from modules.datos_df_cargados import mostrar_estadisticas_df
 
 def display():
     st.markdown("<h2 style='text-align: center;'>Página de análisis exploratorio de datos (EDA)</h2>", unsafe_allow_html=True)
@@ -13,13 +14,13 @@ def display():
         st.image('sources/mapa_aviones_usa.png')
     
     st.markdown("---")
-
     col1, col2 = st.columns([1, 4])
 
     with col1:
         st.write("Puedes seleccionar el mes de diciembre de los años 2021, 2022 y 2023, o bien elegir todos los años para ver todos los datos juntos.")
         df_seleccionado = seleccionar_datos()
         if df_seleccionado is not None:
+            mostrar_estadisticas_df(df_seleccionado, 'fecha')  # Asumiendo que la columna de fecha se llama 'fecha'
             st.write("Una vez que has elegido el año, ahora puedes elegir qué tipo de gráfica ver. Para ayudarte hemos agrupado las gráficas por categorías.")
             grafica_funcion = seleccionar_grafica()
 
