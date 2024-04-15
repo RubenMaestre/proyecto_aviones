1add4c4eb4f0ef7c64d2ff6ed2169cd798da4c35
RubenMaestre <formacion@rubenmaestre.com>
2024-04-15 10:07:25 +0200

    actualiza

--- paginas/eda.py
+++ paginas/eda.py
@@ -10,6 +10,9 @@ from paginas.anual.todos_3 import display as display_todos_3
 from paginas.anual.dec_2023 import display as display_2023
 from paginas.anual.dec_2022 import display as display_2022
 from paginas.anual.dec_2021 import display as display_2021
+from paginas.anual.dec_2023_2 import display as display_2023_2
+from paginas.anual.dec_2022_2 import display as display_2022_2
+from paginas.anual.dec_2021_2 import display as display_2021_2
 
 def display():
     st.title('Exploración de Datos de Vuelos')
@@ -26,10 +29,16 @@ def display():
     if 'subpagina_eda' in st.session_state:
         if st.session_state.subpagina_eda == '2023':
             display_2023()
+        elif st.session_state.subpagina_eda == '2023_2':
+            display_2023_2()
         elif st.session_state.subpagina_eda == '2022':
             display_2022()
+        elif st.session_state.subpagina_eda == '2022_2':
+            display_2022_2()
         elif st.session_state.subpagina_eda == '2021':
             display_2021()
+        elif st.session_state.subpagina_eda == '2021_2':
+            display_2021_2()
         elif st.session_state.subpagina_eda == 'todos_1':
             display_todos_1()
         elif st.session_state.subpagina_eda == 'todos_2':
