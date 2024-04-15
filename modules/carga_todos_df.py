6a0da3d9611530fbd4f4bad7da4b9e0ae29396ac
RubenMaestre <formacion@rubenmaestre.com>
2024-04-15 09:27:53 +0200

    actualiza

--- modules/carga_todos_df.py
+++ modules/carga_todos_df.py
@@ -1,28 +1,16 @@
 import pandas as pd
 import streamlit as st
 
-@st.cache_data
+@st.cache
 def cargar_todos_df():
-    # Define los años y la cantidad de archivos Parquet por año
-    anios = {
-        '2023': 2,
-        '2022': 2,
-        '2021': 2
-    }
+    # Ruta al archivo único Parquet
+    archivo = 'data/pickles/vuelos_limpio.parquet'
     
     if 'df_todos' not in st.session_state:
-        dataframes = []
-        
-        # Iterar sobre cada año y cargar cada archivo Parquet correspondiente
-        for anio, num_archivos in anios.items():
-            for i in range(1, num_archivos + 1):
-                archivo = f'data/parquet/df_{anio}_{i}.parquet'
-                df_temp = pd.read_parquet(archivo)
-                dataframes.append(df_temp)
-        
-        # Concatenar todos los DataFrames en uno solo
-        st.session_state.df_todos = pd.concat(dataframes, ignore_index=True)
+        # Cargar el DataFrame desde el archivo Parquet
+        st.session_state.df_todos = pd.read_parquet(archivo)
 
     # Devolver el DataFrame desde el estado de sesión
     return st.session_state.df_todos
+
 
