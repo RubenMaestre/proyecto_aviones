619f63032ce4c88b2f7a2ae0ff959c565daa694a
RubenMaestre <formacion@rubenmaestre.com>
2024-04-16 17:44:26 +0200

    Update selector_estado.py

--- modules/map/selector_estado.py
+++ modules/map/selector_estado.py
@@ -9,7 +9,6 @@ def mostrar_mapa_aeropuertos_por_estado(key_suffix=''):
     geojson_usa = 'data/us-states.json'
     nombres_estados = df_aeropuertos_unicos['nombre_estado'].unique()
 
-    # Asegúrate de pasar la clave con sufijo para evitar conflictos
     nombre_estado_seleccionado = st.selectbox(
         'Selecciona un estado:',
         sorted(nombres_estados),
@@ -17,34 +16,34 @@ def mostrar_mapa_aeropuertos_por_estado(key_suffix=''):
     )
 
     if st.button('Mostrar Mapa', key=f'btn_mostrar_mapa{key_suffix}'):
-        # Función para actualizar el mapa con los aeropuertos del nombre del estado seleccionado
-        def actualizar_mapa(nombre_estado):
-            unique_key = str(uuid.uuid4())  # Generar un UUID único para cada mapa para evitar colisiones de clave
-            df_aeropuertos_estado = df_aeropuertos_unicos[df_aeropuertos_unicos['nombre_estado'] == nombre_estado]
-            centro_estado = [df_aeropuertos_estado['latitude'].mean(), df_aeropuertos_estado['longitude'].mean()]
-            mapa_estado = folium.Map(location=centro_estado, zoom_start=5)
-            folium.GeoJson(
-                geojson_usa,
-                name='USA States',
-                style_function=lambda feature: {
-                    'fillColor': '#aaffaa',
-                    'color': 'black',
-                    'weight': 2,
-                    'dashArray': '5, 5'
-                },
-                highlight_function=lambda feature: {
-                    'fillColor': '#ffafaa',
-                    'color': 'red',
-                    'weight': 3,
-                }
-            ).add_to(mapa_estado)
-            for index, row in df_aeropuertos_estado.iterrows():
-                folium.Marker(
-                    [row['latitude'], row['longitude']],
-                    popup=f"{row['nombre_aeropuerto']} ({row['codigo_aeropuerto']})",
-                    icon=folium.Icon(color='blue', icon='plane', prefix='fa')
-                ).add_to(mapa_estado)
-            st_folium(mapa_estado, width=1280, height=720, key=f"map_{unique_key}")
+        st.session_state['map_triggered'] = True
 
-        
+    if 'map_triggered' in st.session_state and st.session_state['map_triggered']:
+        unique_key = str(uuid.uuid4())  # Generar un UUID único para cada mapa para evitar colisiones de clave
+        df_aeropuertos_estado = df_aeropuertos_unicos[df_aeropuertos_unicos['nombre_estado'] == nombre_estado_seleccionado]
+        centro_estado = [df_aeropuertos_estado['latitude'].mean(), df_aeropuertos_estado['longitude'].mean()]
+        mapa_estado = folium.Map(location=centro_estado, zoom_start=5)
+        folium.GeoJson(
+            geojson_usa,
+            name='USA States',
+            style_function=lambda feature: {
+                'fillColor': '#aaffaa',
+                'color': 'black',
+                'weight': 2,
+                'dashArray': '5, 5'
+            },
+            highlight_function=lambda feature: {
+                'fillColor': '#ffafaa',
+                'color': 'red',
+                'weight': 3,
+            }
+        ).add_to(mapa_estado)
+        for index, row in df_aeropuertos_estado.iterrows():
+            folium.Marker(
+                [row['latitude'], row['longitude']],
+                popup=f"{row['nombre_aeropuerto']} ({row['codigo_aeropuerto']})",
+                icon=folium.Icon(color='blue', icon='plane', prefix='fa')
+            ).add_to(mapa_estado)
+        st_folium(mapa_estado, width=1280, height=720, key=f"map_{unique_key}")
+
 
