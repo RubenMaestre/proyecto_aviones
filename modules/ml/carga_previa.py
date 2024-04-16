b6ca235efa04cb931ab22abc36d7e5a2a8e85292
RubenMaestre <formacion@rubenmaestre.com>
2024-04-16 16:01:00 +0200

    actualiza

--- modules/ml/carga_previa.py
+++ modules/ml/carga_previa.py
@@ -2,6 +2,8 @@ import pandas as pd
 import streamlit as st
 import pickle
 from joblib import load
+import requests
+from io import BytesIO
 from modules.carga_todos_df import cargar_todos_df
 from modules.ml.import_df import cargar_df_modelo
 from modules.ml.rutas_unicas import obtener_rutas_unicas
@@ -17,10 +19,16 @@ def carga_inicial():
         st.session_state.rutas = obtener_rutas_unicas()
 
     if 'model' not in st.session_state:
-        st.session_state.model = load('https://www.rubenmaestre.com/modelos/model.joblib')
+        # Descargar el modelo de la URL
+        response = requests.get('https://www.rubenmaestre.com/modelos/model.joblib')
+        # Asegurarte de que la solicitud fue exitosa
+        response.raise_for_status()
+        # Cargar el modelo desde los datos descargados
+        st.session_state.model = load(BytesIO(response.content))
 
     if 'mappings' not in st.session_state:
         with open('data/target_encodings.pkl', 'rb') as file:
             st.session_state.mappings = pickle.load(file)
 
     return st.session_state.df_todos, st.session_state.df_modelo, st.session_state.rutas, st.session_state.model, st.session_state.mappings
+
