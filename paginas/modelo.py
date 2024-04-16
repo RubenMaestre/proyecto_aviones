fbe16046720eb811a544cba0041f2d4f74d1bd05
RubenMaestre <formacion@rubenmaestre.com>
2024-04-16 13:08:57 +0200

    Update modelo.py

--- paginas/modelo.py
+++ paginas/modelo.py
@@ -1,6 +1,6 @@
 #paginas/modelo.py
 import streamlit as st
-from modules.ml.ml_2 import display_ml_page
+#from modules.ml.ml_2 import display_ml_page
 
 def display():
     st.title('Modelo machine learning')
@@ -9,4 +9,4 @@ def display():
     st.markdown("""
     Haz tu selección para saber si tu vuelo llegará con retraso o no con nuestro infalible modelo de prediccón...
     """)
-    display_ml_page()
+    #display_ml_page()
