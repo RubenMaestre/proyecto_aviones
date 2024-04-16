e3717299c51021c43374d588b4b130e2d4d53a9f
RubenMaestre <formacion@rubenmaestre.com>
2024-04-16 15:29:02 +0200

    actualiza

--- modules/ml/prediccion.py
+++ modules/ml/prediccion.py
@@ -3,14 +3,12 @@ import pandas as pd
 import streamlit as st
 import joblib
 
-# Función para cargar el modelo
 def cargar_modelo(path):
-    return joblib.load(path)
+    # Esta función carga el modelo desde un archivo joblib
+    modelo = joblib.load(path)
+    return modelo
 
 # Función para realizar una predicción
 def hacer_prediccion(modelo, datos):
     return modelo.predict(datos)
 
-# Función para cargar datos
-def cargar_datos(path):
-    return pd.read_parquet(path)
--- paginas/modelo.py
+++ paginas/modelo.py
@@ -1,6 +1,7 @@
 #paginas/modelo.py
 import streamlit as st
-from modules.ml.prediccion import cargar_modelo, hacer_prediccion, cargar_datos
+import streamlit as st
+from modules.ml.prediccion import cargar_modelo, hacer_prediccion
 
 def display():
     st.title('Modelo de Machine Learning')
@@ -8,13 +9,8 @@ def display():
     Haz tu selección para saber si tu vuelo llegará con retraso o no con nuestro infalible modelo de predicción...
     """)
 
-    # Cargar el modelo y los datos
-    modelo = cargar_modelo('data/joblib/model.joblib')
-    datos = cargar_datos('data/df_modelo.parquet')
-
-    # Añade aquí código para capturar la entrada del usuario y realizar predicciones
-    # Por ejemplo, podrías tener un formulario para que los usuarios ingresen los detalles del vuelo
-
-    if st.button('Predice el retraso'):
-        resultado = hacer_prediccion(modelo, datos)  # Asegúrate de ajustar esta llamada a las necesidades reales
-        st.write(f"Resultado de la predicción: {resultado}")
+    # Botón para cargar el modelo y hacer una predicción
+    if st.button('Cargar modelo y predecir retraso'):
+        # Cargar el modelo solo cuando el usuario lo solicite
+        modelo = cargar_modelo('data/joblib/model.joblib')
+        st.write("Modelo cargado correctamente.")
