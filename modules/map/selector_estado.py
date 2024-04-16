67c37b3552f9e03572a4bcdc36e25789cb8a8554
RubenMaestre <formacion@rubenmaestre.com>
2024-04-16 18:01:09 +0200

    Update selector_estado.py

--- modules/map/selector_estado.py
+++ modules/map/selector_estado.py
@@ -7,26 +7,18 @@ def mostrar_mapa_aeropuertos_por_estado(key_suffix=''):
     df_aeropuertos_unicos = pd.read_pickle('data/aeropuertos_unicos.pkl')
     geojson_usa = 'data/us-states.json'
     nombres_estados = df_aeropuertos_unicos['nombre_estado'].unique()
-    
-    # Asegúrate de pasar la clave con sufijo para evitar conflictos
+
     nombre_estado_seleccionado = st.selectbox(
         'Selecciona un estado:',
         sorted(nombres_estados),
         key=f'estado_seleccionado{key_suffix}'
     )
 
-    # Función para actualizar el mapa con los aeropuertos del nombre del estado seleccionado
     def actualizar_mapa(nombre_estado):
-        # Filtrar aeropuertos por el nombre del estado seleccionado
         df_aeropuertos_estado = df_aeropuertos_unicos[df_aeropuertos_unicos['nombre_estado'] == nombre_estado]
-
-        # Encuentra el centro del estado para centrar el mapa
         centro_estado = [df_aeropuertos_estado['latitude'].mean(), df_aeropuertos_estado['longitude'].mean()]
-
-        # Crea un nuevo mapa centrado en el estado seleccionado
         mapa_estado = folium.Map(location=centro_estado, zoom_start=5)
 
-        # Agregar GeoJSON para marcar los estados
         folium.GeoJson(
             geojson_usa,
             name='USA States',
@@ -43,7 +35,6 @@ def mostrar_mapa_aeropuertos_por_estado(key_suffix=''):
             }
         ).add_to(mapa_estado)
 
-        # Agrega marcadores para cada aeropuerto en el estado
         for index, row in df_aeropuertos_estado.iterrows():
             folium.Marker(
                 [row['latitude'], row['longitude']],
@@ -51,11 +42,8 @@ def mostrar_mapa_aeropuertos_por_estado(key_suffix=''):
                 icon=folium.Icon(color='blue', icon='plane', prefix='fa')
             ).add_to(mapa_estado)
 
-        # Muestra el mapa actualizado en Streamlit
-        st_folium(mapa_estado, width=1280, height=720)
+        st_folium(mapa_estado, width=1280, height=720, key=f'mapa_aeropuertos{key_suffix}')
 
-    # Actualizar el mapa cuando se selecciona un nombre de estado
     actualizar_mapa(nombre_estado_seleccionado)
 
-# Llamar a la función para mostrar el mapa
 mostrar_mapa_aeropuertos_por_estado(key_suffix='_pagina_principal')
