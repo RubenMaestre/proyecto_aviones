96cec0c22a70801ee27da46fb112b5d7bf90da75
RubenMaestre <formacion@rubenmaestre.com>
2024-04-18 19:58:31 +0200

    actualiza

--- modules/graph/s_top_estados_mas_retrasos.py
+++ modules/graph/s_top_estados_mas_retrasos.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from modules.graph.diccionario_estados import estados_EEUU  # Asegúrate de que la ruta de importación sea correcta
 
 def graficar_estados_mas_retrasos(df):
-    # Calculo de retrasos
-    retrasos_llegada = df[df['retraso_llegada'] > 15]
-    retrasos_estados_origen = retrasos_llegada.groupby(['estado_origen']).size().reset_index(name='con_retraso')
+    # Suponiendo que df ya tiene una columna 'estado_origen' con códigos de estado
+    df['estado_origen'] = df['estado_origen'].map(estados_EEUU).fillna(df['estado_origen'])
 
-    # Filtrado para obtener los top 10 estados con más retrasos
+    # Filtrado y cálculo como antes
+    retrasos_estados_origen = df[df['retraso_llegada'] > 15].groupby('estado_origen').size().reset_index(name='con_retraso')
     top_estados_mas_retrasos = retrasos_estados_origen.nlargest(10, 'con_retraso')
 
-    # Gráfica
+    # Crear gráfica
     fig = px.bar(top_estados_mas_retrasos, 
                  x='estado_origen', 
                  y='con_retraso', 
@@ -23,7 +24,3 @@ def graficar_estados_mas_retrasos(df):
 
     # Mostrar la figura en la aplicación Streamlit
     st.plotly_chart(fig)
-
-# Asumir que df ya está cargado o se carga en otra parte del código
-# df = pd.read_csv('ruta_del_archivo.csv')
-# graficar_estados_mas_retrasos(df)
--- modules/graph/t_top_menos_retrasos.py
+++ modules/graph/t_top_menos_retrasos.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from modules.graph.diccionario_estados import estados_EEUU 
 
 def graficar_estados_menos_retrasos(df):
-    # Calculo de retrasos
-    retrasos_llegada = df[df['retraso_llegada'] > 15]
-    retrasos_estados_origen = retrasos_llegada.groupby(['estado_origen']).size().reset_index(name='con_retraso')
+    # Aplicar el mapeo del diccionario para convertir códigos de estado a nombres completos
+    df['estado_origen'] = df['estado_origen'].map(estados_EEUU).fillna(df['estado_origen'])
 
-    # Filtrado para obtener los 10 estados con menos retrasos
+    # Calcular los retrasos
+    retrasos_llegada = df[df['retraso_llegada'] > 15]
+    retrasos_estados_origen = retrasos_llegada.groupby('estado_origen').size().reset_index(name='con_retraso')
+
+    # Filtrar para obtener los 10 estados con menos retrasos
     top_estados_menos_retrasos = retrasos_estados_origen.nsmallest(10, 'con_retraso')
 
-    # Gráfica
+    # Crear la gráfica
     fig = px.bar(top_estados_menos_retrasos, 
                  x='estado_origen', 
                  y='con_retraso', 
