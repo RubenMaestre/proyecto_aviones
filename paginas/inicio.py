15de297cface8cce8f61ffc3db70f07226901a15
RubenMaestre <formacion@rubenmaestre.com>
2024-04-18 21:49:01 +0200

    Update inicio.py

--- paginas/inicio.py
+++ paginas/inicio.py
@@ -9,8 +9,8 @@ def display():
 
     st.markdown("""
         ### Proyecto final para Hack a Boss
-
-        Este proyecto constituye el punto culminante de nuestra formación en el Bootcamp de Data Science e Inteligencia Artificial en Hack a Boss. En nuestro tercer y último proyecto, hemos decidido abordar un desafío crítico y de alta aplicabilidad en el sector aeronáutico: la optimización de la puntualidad y la gestión de retrasos en vuelos operados desde y hacia aeropuertos en Estados Unidos. A través de un exhaustivo análisis exploratorio de datos, este estudio ofrece perspectivas sobre causas, desempeños y factores influyentes en la puntualidad aérea, proporcionando un modelo predictivo en cuanto a la puntualidad de las aerolíneas para mejorar la satisfacción de los pasajeros.
+        """)
+    st.write("""Este proyecto constituye el punto culminante de nuestra formación en el Bootcamp de Data Science e Inteligencia Artificial en Hack a Boss. En nuestro tercer y último proyecto, hemos decidido abordar un desafío crítico y de alta aplicabilidad en el sector aeronáutico: la optimización de la puntualidad y la gestión de retrasos en vuelos operados desde y hacia aeropuertos en Estados Unidos. A través de un exhaustivo análisis exploratorio de datos, este estudio ofrece perspectivas sobre causas, desempeños y factores influyentes en la puntualidad aérea, proporcionando un modelo predictivo en cuanto a la puntualidad de las aerolíneas para mejorar la satisfacción de los pasajeros.
         ---
     """)
 
@@ -19,8 +19,7 @@ def display():
     with col1:
         st.markdown("""
             #### El Equipo
-        """)
-        st.write("""
+        
         Este estudio ha sido desarrollado por José Núñez, Rubén Maestre, Dafne Moreno y Nahuel Núñez. Como equipo, hemos combinado nuestras competencias técnicas y experiencia analítica para investigar los retrasos en los vuelos, un tema crítico en la gestión aeronáutica. A través del análisis de grandes volúmenes de datos, hemos identificado tendencias significativas y patrones de comportamiento que influyen en la puntualidad aérea. Nuestro trabajo se centra en proporcionar un entendimiento detallado de los factores que afectan los tiempos de vuelo, con el objetivo de contribuir a la mejora de las operaciones en aeropuertos estadounidenses.
 
         Nuestro enfoque sistemático nos ha permitido desglosar los elementos clave que contribuyen a los retrasos aéreos, evaluando cómo variaciones en la gestión de aerolíneas, la infraestructura aeroportuaria y condiciones externas como festividades y clima impactan en la eficiencia operacional. Las conclusiones derivadas de este análisis proporcionan una base para la formulación de estrategias que pueden ser implementadas por entidades del sector para optimizar la puntualidad de sus operaciones.
@@ -40,7 +39,6 @@ def display():
 
     with col3:
         st.markdown("""
-        ---
         #### Herramientas y Tecnologías
 
         Para la realización de este proyecto, hemos empleado una variedad de herramientas tecnológicas adquiridas durante nuestro curso, incluyendo Python, Pandas, Numpy, Plotly, Matplotlib y Keras. Estas herramientas han sido fundamentales para procesar y analizar grandes volúmenes de datos de manera eficiente, así como para la visualización clara e interactiva de los resultados, facilitando el entendimiento de las tendencias y patrones detectados.
@@ -49,4 +47,4 @@ def display():
     """)
 
     with col4:
-        st.image("sources/logotipo_hack_a_boss.png", caption="Logo de Hack a Boss")
+        st.image("sources/logotipo_hack_a_boss.png")
