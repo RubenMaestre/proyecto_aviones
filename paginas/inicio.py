ad7c99ea56af328cad5ac92249316c762715e3ba
RubenMaestre <formacion@rubenmaestre.com>
2024-04-18 22:00:06 +0200

    Update inicio.py

--- paginas/inicio.py
+++ paginas/inicio.py
@@ -7,18 +7,24 @@ def display():
     # Título
     st.title("Estudio de puntualidad aérea: Un análisis en profundidad sobre la puntualidad en los aeropuertos estadounidenses")
 
-    st.markdown("""
-        ### Proyecto final para Hack a Boss
+    col1, col2 = st.columns([5,2])
+    with col1:
+        st.markdown("""
+            ### Proyecto final para Hack a Boss
+            """)
+        st.markdown("""##### Este proyecto constituye el punto culminante de nuestra formación en el Bootcamp de Data Science e Inteligencia Artificial en Hack a Boss. En nuestro tercer y último proyecto, hemos decidido abordar un desafío crítico y de alta aplicabilidad en el sector aeronáutico: la optimización de la puntualidad y la gestión de retrasos en vuelos operados desde y hacia aeropuertos en Estados Unidos. A través de un exhaustivo análisis exploratorio de datos, este estudio ofrece perspectivas sobre causas, desempeños y factores influyentes en la puntualidad aérea, proporcionando un modelo predictivo en cuanto a la puntualidad de las aerolíneas para mejorar la satisfacción de los pasajeros.
+            
         """)
-    st.markdown("""##### Este proyecto constituye el punto culminante de nuestra formación en el Bootcamp de Data Science e Inteligencia Artificial en Hack a Boss. En nuestro tercer y último proyecto, hemos decidido abordar un desafío crítico y de alta aplicabilidad en el sector aeronáutico: la optimización de la puntualidad y la gestión de retrasos en vuelos operados desde y hacia aeropuertos en Estados Unidos. A través de un exhaustivo análisis exploratorio de datos, este estudio ofrece perspectivas sobre causas, desempeños y factores influyentes en la puntualidad aérea, proporcionando un modelo predictivo en cuanto a la puntualidad de las aerolíneas para mejorar la satisfacción de los pasajeros.
-        
-    """)
+    with col2:
+        st.markdown("<br><br>", unsafe_allow_html=True)
+        st.image("sources/logotipo_hack_a_boss.png")
+
 
     st.markdown("<br><br><br>", unsafe_allow_html=True)
 
-    col1, col2, col3 = st.columns([5, 1, 5])
+    col3, col4, col5 = st.columns([5, 1, 5])
     
-    with col1:
+    with col3:
         st.markdown("""
             #### El Equipo
         
@@ -27,10 +33,10 @@ def display():
         Nuestro enfoque sistemático nos ha permitido desglosar los elementos clave que contribuyen a los retrasos aéreos, evaluando cómo variaciones en la gestión de aerolíneas, la infraestructura aeroportuaria y condiciones externas como festividades y clima impactan en la eficiencia operacional. Las conclusiones derivadas de este análisis proporcionan una base para la formulación de estrategias que pueden ser implementadas por entidades del sector para optimizar la puntualidad de sus operaciones.
         """)
     
-    with col2:
+    with col4:
         st.write("")
 
-    with col3:
+    with col5:
         st.markdown("""
             #### Metodología y Objetivos
 
@@ -40,11 +46,8 @@ def display():
         """)
 
     st.markdown("<br><br><br>", unsafe_allow_html=True)
-
-    col4, col5 = st.columns([5,2])
-
-    with col4:
-        st.markdown("""
+    
+    st.markdown("""
         #### Herramientas y Tecnologías
 
         Para la realización de este proyecto, hemos empleado una variedad de herramientas tecnológicas adquiridas durante nuestro curso, incluyendo Python, Pandas, Numpy, Plotly, Matplotlib y Keras. Estas herramientas han sido fundamentales para procesar y analizar grandes volúmenes de datos de manera eficiente, así como para la visualización clara e interactiva de los resultados, facilitando el entendimiento de las tendencias y patrones detectados.
@@ -52,6 +55,4 @@ def display():
         Mediante el uso de Streamlit, hemos logrado presentar nuestros resultados de manera dinámica y accesible, proporcionando a los usuarios la posibilidad de interactuar con los datos y explorar en profundidad las diversas facetas de nuestro análisis. Este proyecto refleja no solo el aprendizaje alcanzado en Hack a Boss, sino también nuestra determinación por aplicar la ciencia de datos en la solución de problemas prácticos del mundo real. Confiamos en que los insights y visualizaciones que ofrecemos sean tanto informativos como de utilidad práctica para los interesados.
     """)
 
-    with col5:
-        st.markdown("<br><br>", unsafe_allow_html=True)
-        st.image("sources/logotipo_hack_a_boss.png")
+    
