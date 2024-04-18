0cdd7973b983794cf0c834b74329b194c7bbc549
RubenMaestre <formacion@rubenmaestre.com>
2024-04-18 17:58:49 +0200

    actualiza

--- modules/graph/g_correlacion_lineal.py
+++ modules/graph/g_correlacion_lineal.py
@@ -1,26 +1,32 @@
 import streamlit as st
 import plotly.express as px
+import pandas as pd
 
 def graficar_correlacion_lineal(df):
     # Verificar y manejar valores nulos
     if df["retraso_salida"].isnull().any() or df["retraso_llegada"].isnull().any():
         st.warning("Advertencia: Se detectaron valores nulos en las columnas de datos.")
         df = df.dropna(subset=["retraso_salida", "retraso_llegada"])  # Eliminar valores nulos
+    
+    # Usar una muestra de datos para mantener el rendimiento
+    df_sample = df.sample(n=100000, random_state=42) if len(df) > 100000 else df
 
+    # Crear gráfico de dispersión
     fig = px.scatter(
-        data_frame=df,
+        data_frame=df_sample,
         x="retraso_salida",
         y="retraso_llegada",
         title="Correlación Lineal entre los indicadores de retraso en la salida y en la llegada de vuelos",
         opacity=0.5,
-        trendline='ols',  # Añade una línea de tendencia de mínimos cuadrados ordinarios
-        hover_data={"retraso_salida": True, "retraso_llegada": True}  # Mostrar más datos al pasar el mouse
+        trendline=None  # Remover la línea de tendencia para mejorar el rendimiento
     )
 
+    # Ajustes de visualización y rendimiento
     fig.update_layout(
-        title_x=0.5,  # Centrar el título
-        width=1080,  # Ancho personalizado
-        height=720   # Altura personalizada
+        title_x=0.5,
+        width=900,
+        height=600,
+        showlegend=False
     )
 
     # Mostrar la figura en la aplicación Streamlit
