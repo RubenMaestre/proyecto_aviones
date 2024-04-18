848c5d52da187bce926d18a58ada999894866dca
RubenMaestre <formacion@rubenmaestre.com>
2024-04-18 20:16:23 +0200

    Update y_maxima_distancia_millas.py

--- modules/graph/y_maxima_distancia_millas.py
+++ modules/graph/y_maxima_distancia_millas.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def graficar_maxima_distancia_millas(df):
     # Crear una columna única que represente el par de aeropuertos en ambos sentidos
@@ -12,21 +13,15 @@ def graficar_maxima_distancia_millas(df):
     # Ordenar por distancia de mayor a menor y tomar los top 20
     top_20_distancias = max_distancias.sort_values(by='distancia_millas', ascending=False).head(20)
 
-    # Gráfico de barras con Plotly Express
-    fig = px.bar(top_20_distancias, 
-                 x='par_aeropuertos', 
-                 y='distancia_millas', 
-                 title='Top 20 Máxima Distancia en Millas entre Pares de Aeropuertos Únicos',
-                 labels={'par_aeropuertos': 'Par de Aeropuertos', 'distancia_millas': 'Distancia en Millas'},
-                 color='distancia_millas',
-                 color_continuous_scale=px.colors.sequential.Viridis)
+    # Crear un gráfico de barras usando Matplotlib
+    plt.figure(figsize=(10, 8))  # Configurar el tamaño del gráfico
+    sns.barplot(x='distancia_millas', y='par_aeropuertos', data=top_20_distancias, palette='viridis')
 
-    fig.update_layout(
-        title_x=0.5,
-        xaxis={'categoryorder': 'total descending', 'title': 'Pares de Aeropuertos'},
-        yaxis={'title': 'Máxima Distancia en Millas'},
-        width=1080
-    )
+    plt.title('Top 20 Máxima Distancia en Millas entre Pares de Aeropuertos Únicos')
+    plt.xlabel('Distancia en Millas')
+    plt.ylabel('Par de Aeropuertos')
+    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
+    plt.tight_layout()
 
     # Mostrar la figura en la aplicación Streamlit
-    st.plotly_chart(fig)
+    st.pyplot(plt.gcf())
