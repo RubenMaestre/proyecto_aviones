ab211cf2e52544f927e550d796c3148491e9a13c
RubenMaestre <formacion@rubenmaestre.com>
2024-04-19 18:41:47 +0200

    actualiza

--- modules/eda/graficas.py
+++ modules/eda/graficas.py
@@ -24,38 +24,35 @@ from modules.graph.t_top_menos_retrasos import graficar_estados_menos_retrasos
 from modules.graph.u_diagrama_distancia_millas import graficar_diagrama_distancia_millas
 from modules.graph.v_histograma_distancias_millas import graficar_histograma_distancias_millas
 from modules.graph.w_relacion_retrasos_millas import graficar_relacion_retrasos_millas
-from modules.graph.z_diagrama_caja_retraso_llegada import graficar_diagrama_caja_retrasos
 from modules.graph.x_numero_retrasos_intervalo import graficar_retrasos_por_intervalo
 from modules.graph.y_costo_aerolinea import graficar_vuelos_por_costo_aerolinea
 from modules.graph.yy_aerolinea_costo_retrasos import graficar_vuelos_retrasos_por_costo_aerolinea
 
-# Diccionario
 graficas_por_nombre = {
-    "graficar_vuelos_totales_por_year": graficar_vuelos_totales_por_year,
-    "graficar_evolucion_vuelos_por_aerolinea": graficar_evolucion_vuelos_por_aerolinea,
-    "graficar_vuelos_por_aerolinea": graficar_vuelos_por_aerolinea,
-    "graficar_horas_vuelos": graficar_horas_vuelos,
-    "graficar_mapa_calor_correlacion": graficar_mapa_calor_correlacion,
-    "mostrar_correlaciones_significativas": mostrar_correlaciones_significativas,
-    "graficar_correlacion_lineal": graficar_correlacion_lineal,
-    "graficar_retrasos_mas_15": graficar_retrasos_mas_15,
-    "graficar_retrasos_mas_15_festivos": graficar_retrasos_mas_15_festivos,
-    "graficar_top_aerolineas_con_sin_retrasos": graficar_top_aerolineas_con_sin_retrasos,
-    "graficar_top_aeropuertos_con_sin_retrasos": graficar_top_aeropuertos_con_sin_retrasos,
-    "graficar_numero_vuelos_dias_diciembre": graficar_numero_vuelos_dias_diciembre,
-    "graficar_numero_vuelos_acumulados_diciembre": graficar_numero_vuelos_acumulados_diciembre,
-    "graficar_cantidad_llegadas_salidas_por_hora": graficar_cantidad_llegadas_salidas_por_hora,
-    "graficar_cantidad_retrasos_por_hora": graficar_cantidad_retrasos_por_hora,
-    "graficar_dias_semana_con_sin_retrasos": graficar_dias_semana_con_sin_retrasos,
-    "graficar_total_minutos_por_tipo_retraso": graficar_total_minutos_por_tipo_retraso,
-    "graficar_analisis_retrasos_aereos": graficar_analisis_retrasos_aereos,
-    "graficar_estados_mas_retrasos": graficar_estados_mas_retrasos,
-    "graficar_estados_menos_retrasos": graficar_estados_menos_retrasos,
-    "graficar_diagrama_distancia_millas": graficar_diagrama_distancia_millas,
-    "graficar_histograma_distancias_millas": graficar_histograma_distancias_millas,
-    "graficar_relacion_retrasos_millas": graficar_relacion_retrasos_millas,
-    "graficar_diagrama_caja_retrasos": graficar_diagrama_caja_retrasos,
-    "graficar_retrasos_por_intervalo": graficar_retrasos_por_intervalo,
-    "graficar_vuelos_por_costo_aerolinea": graficar_vuelos_por_costo_aerolinea,
-    "graficar_vuelos_retrasos_por_costo_aerolinea": graficar_vuelos_retrasos_por_costo_aerolinea
+    "Vuelos totales por año": graficar_vuelos_totales_por_year,
+    "Evolución de vuelos por aerolínea": graficar_evolucion_vuelos_por_aerolinea,
+    "Vuelos por aerolínea": graficar_vuelos_por_aerolinea,
+    "Diagrama de salidas y llegadas": graficar_horas_vuelos,
+    "Mapa de calor de correlación": graficar_mapa_calor_correlacion,
+    "Correlaciones específicas significativas": mostrar_correlaciones_significativas,
+    "Correlación lineal entre retrasos": graficar_correlacion_lineal,
+    "Retrasos mayores a 15 minutos": graficar_retrasos_mas_15,
+    "Retrasos mayores a 15 minutos en festivos": graficar_retrasos_mas_15_festivos,
+    "Top aerolíneas con y sin retrasos": graficar_top_aerolineas_con_sin_retrasos,
+    "Top aeropuertos con y sin retrasos": graficar_top_aeropuertos_con_sin_retrasos,
+    "Número de vuelos por días en diciembre": graficar_numero_vuelos_dias_diciembre,
+    "Número de vuelos acumulados en diciembre": graficar_numero_vuelos_acumulados_diciembre,
+    "Cantidad de llegadas y salidas por hora": graficar_cantidad_llegadas_salidas_por_hora,
+    "Cantidad de retrasos por hora": graficar_cantidad_retrasos_por_hora,
+    "Días de la semana con y sin retrasos": graficar_dias_semana_con_sin_retrasos,
+    "Total de minutos por tipo de retraso": graficar_total_minutos_por_tipo_retraso,
+    "Análisis de retrasos aéreos": graficar_analisis_retrasos_aereos,
+    "Estados con más retrasos": graficar_estados_mas_retrasos,
+    "Estados con menos retrasos": graficar_estados_menos_retrasos,
+    "Diagrama de distancia en millas": graficar_diagrama_distancia_millas,
+    "Histograma de distancias en millas": graficar_histograma_distancias_millas,
+    "Relación entre retrasos y millas": graficar_relacion_retrasos_millas,
+    "Número de retrasos por intervalo": graficar_retrasos_por_intervalo,
+    "Vuelos por costo de aerolínea": graficar_vuelos_por_costo_aerolinea,
+    "Retrasos y costos de aerolíneas": graficar_vuelos_retrasos_por_costo_aerolinea
 }
