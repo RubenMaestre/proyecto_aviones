61eb4b0f3e5c241f1ae0356fd317b2cbcd3e32e8
RubenMaestre <formacion@rubenmaestre.com>
2024-04-19 17:27:01 +0200

    Update descripciones_graficas.py

--- modules/eda/descripciones_graficas.py
+++ modules/eda/descripciones_graficas.py
@@ -1,6 +1,15 @@
 # modules/eda/descripciones_graficas.py
 import pandas as pd
 
+def cargar_descripciones():
+    """Carga las descripciones de las gráficas desde un archivo Excel."""
+    try:
+        df_descripciones = pd.read_excel('data/graficas/comentarios.xlsx')
+        return df_descripciones
+    except Exception as e:
+        print(f"Error al cargar las descripciones: {e}")
+        return pd.DataFrame()  # Retorna un DataFrame vacío si hay error
+
 def obtener_descripcion(año, nombre_grafica, df_descripciones):
     """Busca y retorna la descripción para una gráfica específica dada por año y nombre."""
     try:
