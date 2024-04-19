05a8ea122103d7d217c3dc06447c10a135dbef5e
RubenMaestre <formacion@rubenmaestre.com>
2024-04-19 17:25:59 +0200

    actualiza

--- modules/eda/descripciones_graficas.py
+++ modules/eda/descripciones_graficas.py
@@ -1,31 +1,19 @@
 # modules/eda/descripciones_graficas.py
 import pandas as pd
 
-def cargar_descripciones():
-    """Carga las descripciones de las gráficas desde un archivo Excel y verifica las columnas."""
-    try:
-        # Asegúrate de especificar la ruta completa si el archivo no está en el mismo directorio que este script.
-        df_descripciones = pd.read_excel('data/graficas/comentarios.xlsx')
-        # Verifica que las columnas esperadas existan en el DataFrame
-        if 'Año' not in df_descripciones.columns or 'Nombre' not in df_descripciones.columns:
-            print("Las columnas 'Año' o 'Nombre' no están presentes en el DataFrame.")
-            print("Columnas encontradas:", df_descripciones.columns)
-            return pd.DataFrame()
-        return df_descripciones
-    except Exception as e:
-        print(f"Error al cargar las descripciones: {e}")
-        return pd.DataFrame()  # Retorna un DataFrame vacío si hay error
-
 def obtener_descripcion(año, nombre_grafica, df_descripciones):
     """Busca y retorna la descripción para una gráfica específica dada por año y nombre."""
     try:
+        print(f"Buscando descripción para el año: {año}, gráfica: {nombre_grafica}")  # Diagnóstico
         descripcion = df_descripciones[
             (df_descripciones['Año'] == año) & (df_descripciones['Nombre'] == nombre_grafica)
         ]['Descripción'].values
 
         if descripcion.size > 0:
+            print("Descripción encontrada:", descripcion[0])  # Diagnóstico
             return descripcion[0]
         else:
+            print("Descripción no encontrada.")  # Diagnóstico
             return "Descripción no disponible."
     except Exception as e:
         print(f"Error al obtener la descripción: {e}")
